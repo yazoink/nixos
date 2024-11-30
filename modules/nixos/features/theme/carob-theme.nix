5b0c323ae8019bd649f13e4e8e954be0d22af68e
yazoink <yazoink@firemail.cc>
2024-11-30 21:29:11 +1100

    add sddm themes for different resolutions

--- modules/nixos/features/theme/carob-theme.nix
+++ modules/nixos/features/theme/carob-theme.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
       owner = "yazoink";
       repo = "carob-theme";
       rev = "45f336b99f228e30fcd073d66720f11827eb43fd";
-      sha256 = "sha256-8FMyD98YP7faOtJPZ49B5bh4jkhLnoUOdomnBz4wi8c=";
+      sha256 = "sha256-/fnXQ7dp08/KP6Gs/GWY2zn1tw6GdhHrI2L+vcNwwF0=";
     };
 
     dontUnpack = true;
