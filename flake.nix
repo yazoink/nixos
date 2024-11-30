16714517dd6037ab95f12ad274182dce5cdf676e
yazoink <yazoink@firemail.cc>
2024-11-30 22:36:25 +1100

    a

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Gene's NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
