d33556b8fd2df04fecb8466eb69ca9a356c44e34
yazoink <yazoink@firemail.cc>
2024-12-01 02:51:22 +1100

    a

--- modules/home-manager/features/nixvim/plugins.nix
+++ modules/home-manager/features/nixvim/plugins.nix
@@ -149,9 +149,42 @@
           ];
         };
 
-        /*noice = {
+        noice = {
           enable = true;
-        };*/
+          settings = {
+            notify = {
+              enabled = true;
+            };
+            messages = {
+              enabled = true; # Adds a padding-bottom to neovim statusline when set to false for some reason
+            };
+            lsp = {
+              message = {
+                enabled = false;
+              };
+              progress = {
+                enabled = false;
+                view = "mini";
+              };
+            };
+            popupmenu = {
+              enabled = true;
+              backend = "nui";
+            };
+            format = {
+              filter = {
+                pattern = [":%s*%%s*s:%s*" ":%s*%%s*s!%s*" ":%s*%%s*s/%s*" "%s*s:%s*" ":%s*s!%s*" ":%s*s/%s*"];
+                icon = "";
+                lang = "regex";
+              };
+              replace = {
+                pattern = [":%s*%%s*s:%w*:%s*" ":%s*%%s*s!%w*!%s*" ":%s*%%s*s/%w*/%s*" "%s*s:%w*:%s*" ":%s*s!%w*!%s*" ":%s*s/%w*/%s*"];
+                icon = "󱞪";
+                lang = "regex";
+              };
+            };
+          };
+        };
 
         web-devicons.enable = true;
         barbecue.enable = true;
