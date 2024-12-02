ffab0d297c2f87a24bfad3dc5cadf0eda6f45534
yazoink <yazoink@firemail.cc>
2024-12-03 00:03:34 +1100

    ollama

--- modules/nixos/features/amdgpu/default.nix
+++ modules/nixos/features/amdgpu/default.nix
@@ -11,6 +11,7 @@
     environment.variables = {
       LIBVA_DRIVER_NAME = "radeonsi";
       VDPAU_DRIVER = "radeonsi";
+      OLLAMA_ACCELERATION = "rocm";
     };
 
 
@@ -26,12 +27,7 @@
         enable = true;
         enable32Bit = true;
         extraPackages = with pkgs; [
-          amdvlk
-          rocmPackages.clr.icd
           mesa
-        ];
-        extraPackages32 = with pkgs; [
-          driversi686Linux.amdvlk
         ];
       };
     };
