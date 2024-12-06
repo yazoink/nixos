146c04ae5817cceeee284d41dfef5e38148aac64
yazoink <yazoink@firemail.cc>
2024-12-06 23:48:12 +1100

    a

--- modules/home-manager/features/nixvim/plugins.nix
+++ modules/home-manager/features/nixvim/plugins.nix
@@ -5,30 +5,6 @@
       plugins = {
         lualine = {
           enable = true;
-          settings = {
-            alwaysDivideMiddle = true;
-            ignoreFocus = [ "neo-tree" ];
-            componentSeparators = {
-              left = "|";
-              right = "|";
-            };
-            sectionSeparators = {
-              left = "";
-              right = "";
-            };
-            sections = {
-              lualine_a = [ "mode" ];
-              lualine_b = [
-                "branch"
-                "diff"
-                "diagnostics"
-              ];
-              lualine_c = [ "filename" ];
-              lualine_x = [ "filetype" ];
-              lualine_y = [ "progress" ];
-              lualine_z = [ ''" " .. os.date("%R")'' ];
-            };
-
             options = {
               theme = "auto";
             };
