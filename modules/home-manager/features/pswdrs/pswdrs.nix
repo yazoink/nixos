425b1246777cf1e4a632a63454e9d7a0506799a8
yazoink <yazoink@firemail.cc>
2024-12-07 14:19:18 +1100

    a

--- modules/home-manager/features/pswdrs/pswdrs.nix
+++ modules/home-manager/features/pswdrs/pswdrs.nix
@@ -6,9 +6,13 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "yazoink";
     repo = "pswdrs";
     rev = "main";
-    sha256 = "sha256-wV43NiGYpxuTY2OVQofx2w2IxzJDqKz6hwxBq21oEU0=";
+    sha256 = "sha256-wV43NiGYpxuTY2OVQtfx2w2IxzJDqKz6hwxBq21oEU0=";
   };
   cargoHash = "sha256-3cdwL9ocrSybseg15Bsg4GdE96tvMbilDnpQHRJ+z0c=";
   buildInputs = [];
   nativeBuildInputs = [];
+  postInstall = ''
+    mkdir -p $out/share/pswdrs
+    cp -r $src/data/* $out/share/pswdrs
+  '';
 }
