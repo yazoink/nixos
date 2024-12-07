aa9b55919d9ed28a759e47887e79c795edd2bbd0
yazoink <yazoink@firemail.cc>
2024-12-07 14:43:01 +1100

    a

--- modules/home-manager/features/etterna/local-install.sh
+++ modules/home-manager/features/etterna/local-install.sh
@@ -36,7 +36,7 @@ if [[ -f "$applicationsDir/etterna.desktop" ]]; then
 fi
 
 cp "$2" "$applicationsDir/etterna.desktop"
-echo "Exec=$installDir/Etterna" | tee -a "$applicationsDir/etterna.desktop"
+echo "Exec=$installDir/Etterna" >> "$applicationsDir/etterna.desktop"
 
 chown -R "$3":users "$homeDir/.etterna"
 chmod -R 755 "$homeDir/.etterna"
