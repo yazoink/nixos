d4c708e4ddf0168d61ba295377bcc83f44f7dfbf
yazoink <yazoink@firemail.cc>
2024-12-07 14:29:51 +1100

    a

--- modules/home-manager/features/pswdrs/default.nix
+++ modules/home-manager/features/pswdrs/default.nix
@@ -9,8 +9,8 @@ in
       default = false;
     };
   };
+  home.file.".config/pswdrs/words.txt".source = lib.mkIf config.bundles.base.pswdrs.enable ./words.txt;
   config = lib.mkIf config.bundles.base.pswdrs.enable {
     home.packages = [pswdrs];
   };
-  home.file.".config/pswdrs/words.txt".source = ./words.txt;
 }
