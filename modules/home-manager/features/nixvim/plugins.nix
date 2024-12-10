c120d6ef09452b8dadb856f8cf592e7f282e8090
yazoink <yazoink@firemail.cc>
2024-12-10 20:03:55 +1100

    a

--- modules/home-manager/features/nixvim/plugins.nix
+++ modules/home-manager/features/nixvim/plugins.nix
@@ -9,14 +9,8 @@
             alwaysDivideMiddle = true;
             ignoreFocus = [ "neo-tree" ];
             options = {
-              sectionSeparators = {
-                left = "|";
-                right = "|";
-              };
-              componentSeparators = {
-                left = "|";
-                right = "|";
-              };
+              section_separators = { left = "|"; right = "|"; };
+              component_separators = { left = "|"; right = "|"; };
               theme = "auto";
             };
           };
