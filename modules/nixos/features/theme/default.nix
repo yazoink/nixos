3a2d73848ecce4b850643fbc3e7429a4457a8940
yazoink <yazoink@firemail.cc>
2024-12-11 14:18:21 +1100

    a

--- modules/nixos/features/theme/default.nix
+++ modules/nixos/features/theme/default.nix
@@ -26,7 +26,6 @@ in
         name = "Bibata-Carob";
         size = 24;
       };
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/caroline.yaml";
       /*base16Scheme = {
         base00 = "242120";
         base01 = "2b2726";
@@ -45,6 +44,24 @@ in
         base0E = "998396";
         base0F = "ab9382";
       };*/
+      base16Scheme = {
+        base00 = "1c1213";
+        base01 = "3a2425";
+        base02 = "563837";
+        base03 = "6d4745";
+        base04 = "8b5d57";
+        base05 = "a87569";
+        base06 = "c58d7b";
+        base07 = "e3a68c";
+        base08 = "c24f57";
+        base09 = "a63650";
+        base0A = "f28171";
+        base0B = "806c61";
+        base0C = "6b6566";
+        base0D = "684c59";
+        base0E = "a63650";
+        base0F = "893f45";
+      };
       image = if config.networking.hostName == "cyberia" then ./wallpaper-cyberia.png else ./wallpaper-fluoride.png;
       fonts = {
         sizes = {
