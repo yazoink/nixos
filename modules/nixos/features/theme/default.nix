26104fb322e94db0ca752605562b0f16fc2a93be
yazoink <yazoink@firemail.cc>
2024-12-13 11:53:02 +1100

    a

--- modules/nixos/features/theme/default.nix
+++ modules/nixos/features/theme/default.nix
@@ -7,8 +7,8 @@ let
   #kirsch = pkgs.callPackage ./kirsch {};
   #castella = pkgs.callPackage ./castella {};
   #triskweline = pkgs.callPackage ./triskweline {};
-  #necAcpIII = pkgs.callPackage ./nec-acp-3 {};
-  ibmModel3x = pkgs.callPackage ./ibm-model3x {};
+  necAcpIII = pkgs.callPackage ./nec-acp-3 {};
+  #ibmModel3x = pkgs.callPackage ./ibm-model3x {};
 in
 {
   options = {
@@ -89,8 +89,8 @@ in
         monospace = {
           #package = pkgs.gohufont;
           #name = "Gohufont";
-          package = ibmModel3x;
-          name = "Bm437 IBM Model3x Alt4";
+          package = necAcpIII;
+          name = "Bm437 NEC APC3 8x13";
         };
       };
     };
