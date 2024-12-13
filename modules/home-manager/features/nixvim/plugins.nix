b18f5d47c613edc1f9e68e902af46cb3c64dff01
yazoink <yazoink@firemail.cc>
2024-12-13 12:46:25 +1100

    a

--- modules/home-manager/features/nixvim/plugins.nix
+++ modules/home-manager/features/nixvim/plugins.nix
@@ -74,31 +74,31 @@
             lualine_y = [ "" ];
             lualine_z = [ "" ];
           };
+          luaConfig.post = ''
+            vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
+            vim.api.nvim_create_autocmd("User", {
+              group = "lualine_augroup",
+              pattern = {"LspProgressStatusUpdated", "BufEnter"},
+              callback = require("lualine").refresh
+            })
+
+            vim.api.nvim_create_autocmd({ "RecordingEnter" }, {
+              group = "lualine_augroup",
+              callback = function()
+                require("lualine").hide()
+                local macro = vim.fn.reg_recording()
+                vim.opt.statusline = string.format("%%1*recording @%s", macro)
+              end
+            })
+
+            vim.api.nvim_create_autocmd({ "RecordingLeave" }, {
+              group = "lualine_augroup",
+              callback = function()
+                require("lualine").hide({ unhide = true })
+              end
+            })
+          '';
         };
-        extraConfigLua = ''
-          vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-          vim.api.nvim_create_autocmd("User", {
-            group = "lualine_augroup",
-            pattern = {"LspProgressStatusUpdated", "BufEnter"},
-            callback = require("lualine").refresh
-          })
-
-          vim.api.nvim_create_autocmd({ "RecordingEnter" }, {
-            group = "lualine_augroup",
-            callback = function()
-              require("lualine").hide()
-              local macro = vim.fn.reg_recording()
-              vim.opt.statusline = string.format("%%1*recording @%s", macro)
-            end
-          })
-
-          vim.api.nvim_create_autocmd({ "RecordingLeave" }, {
-            group = "lualine_augroup",
-            callback = function()
-              require("lualine").hide({ unhide = true })
-            end
-          })
-        '';
 
         floaterm = {
           enable = true;
