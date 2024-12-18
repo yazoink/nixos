cfffff7ab0a0ca59e2d2d9ebc6f5ada88a4c09e1
yazoink <yazoink@firemail.cc>
2024-12-19 01:23:39 +1100

    tweak

--- modules/home-manager/features/gtklock/default.nix
+++ modules/home-manager/features/gtklock/default.nix
@@ -1,11 +1,5 @@
 {osConfig, lib, ...}:
 {
-  options = {
-    bundles.base.pswdrs.enable = lib.mkOption {
-      type = lib.types.bool;
-      default = false;
-    };
-  };
   config = lib.mkIf osConfig.bundles.desktopBase.gtklock.enable {
     xdg.configFile."gtklock/config.ini".text = ''
       [main]
