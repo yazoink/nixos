05e240cd5152d33dddc79e0ae0801d52c233e3a9
yazoink <yazoink@firemail.cc>
2024-12-19 21:34:42 +1100

    fix stuff

--- modules/home-manager/features/soundboard/soundboard.nix
+++ modules/home-manager/features/soundboard/soundboard.nix
@@ -7,7 +7,7 @@ pkgs.python312Packages.buildPythonApplication rec {
     owner = "yazoink";
     repo = "soundboard";
     rev = "main";
-    sha256 = "sha256-Xh/ChQHbG8/sgFI9nX+mrPiMJ00RgPM7q2HRKTWU6oo=";
+    sha256 = "sha256-deD+FFHyi3OxWA15ZcE1gOck+plYgT+/FxY+p/ABLt8=";
   };
 
   nativeBuildInputs = [
