1ca8fa086bffd9161bedc4d761976d74a614473a
yazoink <yazoink@firemail.cc>
2024-12-19 12:31:22 +1100

    tweak

--- modules/home-manager/features/color-scripts/default.nix
+++ modules/home-manager/features/color-scripts/default.nix
@@ -1,12 +1,13 @@
 {pkgs, config, lib, ...}:
 let
   colorScripts = pkgs.callPackage ./color-scripts.nix {};
+  dollarSign = "$";
   rcs = pkgs.writeShellScriptBin "rcs" ''
     #!/usr/bin/env bash
 
     ls_output="$(ls ${colorScripts})"
     IFS=" " read -ra scripts <<< "ls_output"
-    index=$(echo $((1 + $RANDOM % $\{#scripts[@]})))
+    index=$(echo $((1 + $RANDOM % ${dollarSign}{#scripts[@]})))
     ${colorScripts}/scripts[index]
   '';
 in
