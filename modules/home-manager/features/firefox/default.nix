0f7fd8619660ec82e00556fc2e9501fdd474ebda
yazoink <yazoink@firemail.cc>
2024-12-20 13:48:20 +1100

    shyfox

--- modules/home-manager/features/firefox/default.nix
+++ modules/home-manager/features/firefox/default.nix
@@ -51,41 +51,41 @@
           SkipOnboarding = true;
         };
       };
-    };
-    profiles."${osConfig.myOptions.userAccount.username}" = {
-      isDefault = true;
-      search = {
-        force = true;
-        default = "4get";
-        privateDefault = "4get";
-        engines = {
-          "4get" = {
-            urls = [{
-              template = "https://4get.yazo.ink/web?s={searchTerms}";
-            }];
-            definedAliases = [ "@4g" ];
+      profiles."${osConfig.myOptions.userAccount.username}" = {
+        isDefault = true;
+        search = {
+          force = true;
+          default = "4get";
+          privateDefault = "4get";
+          engines = {
+            "4get" = {
+              urls = [{
+                template = "https://4get.yazo.ink/web?s={searchTerms}";
+              }];
+              definedAliases = [ "@4g" ];
+            };
           };
         };
+        extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
+          ublock-origin
+          sponsorblock
+          stylus
+          violentmonkey
+          localcdn
+          clearurls
+          foxytab
+          libredirect
+          floccus
+          firefox-color
+          control-panel-for-twitter
+          sidebery
+        ] ++ (if osConfig.networking.hostName == "cyberia" then [enhanced-h264ify] else []);
+        extraConfig = builtins.readFile ./betterfox.js;
+        /*extraConfig = builtins.readFile (builtins.fetchurl {
+          url = "https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js";
+          sha256 = "0vfn3s7q4gm939a5cim1jlrr8k0f5pm4kxxsv1d9bwvv10pmz7pn";
+        });*/
       };
-      extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-        ublock-origin
-        sponsorblock
-        stylus
-        violentmonkey
-        localcdn
-        clearurls
-        foxytab
-        libredirect
-        floccus
-        firefox-color
-        control-panel-for-twitter
-        sidebery
-      ] ++ (if osConfig.networking.hostName == "cyberia" then [enhanced-h264ify] else []);
-      extraConfig = builtins.readFile ./betterfox.js;
-      /*extraConfig = builtins.readFile (builtins.fetchurl {
-        url = "https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js";
-        sha256 = "0vfn3s7q4gm939a5cim1jlrr8k0f5pm4kxxsv1d9bwvv10pmz7pn";
-      });*/
     };
   };
 }
