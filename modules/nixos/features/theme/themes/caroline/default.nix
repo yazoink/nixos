c428256711014950ac8b0d63f1402c4ec975e405
yazoink <yazoink@firemail.cc>
2024-12-23 05:46:33 +1100

    tweak

--- modules/nixos/features/theme/themes/caroline/default.nix
+++ modules/nixos/features/theme/themes/caroline/default.nix
@@ -16,7 +16,7 @@ in
       cursor = {
         name = "caroline-bibata-modern";
       };
-      /*base16Scheme = {
+      base16Scheme = {
         base00 = "1c1213";
         base01 = "3a2425";
         base02 = "563837";
@@ -33,24 +33,6 @@ in
         base0D = "684c59";
         base0E = "a63650";
         base0F = "893f45";
-      };*/
-      base16Scheme = {
-        base00 = "1e1e2e";
-        base01 = "181825";
-        base02 = "313244";
-        base03 = "45475a";
-        base04 = "585b70";
-        base05 = "cdd6f4";
-        base06 = "f5e0dc";
-        base07 = "b4befe";
-        base08 = "f38ba8";
-        base09 = "fab387";
-        base0A = "f9e2af";
-        base0B = "a6e3a1";
-        base0C = "94e2d5";
-        base0D = "89b4fa";
-        base0E = "cba6f7";
-        base0F = "f2cdcd";
       };
     };
   };
