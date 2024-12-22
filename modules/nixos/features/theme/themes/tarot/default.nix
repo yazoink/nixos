fc975af4667b797f35a967369ae20fdf56e6b040
yazoink <yazoink@firemail.cc>
2024-12-23 01:04:00 +1100

    tweak

--- modules/nixos/features/theme/themes/tarot/default.nix
+++ modules/nixos/features/theme/themes/tarot/default.nix
@@ -22,7 +22,7 @@ in
         base02 = "4B2054";
         base03 = "74316B";
         base04 = "8C406F";
-        base05 = "C4686D";
+        base05 = "AA556F";
         base06 = "C4686D";
         base07 = "DC8F7C";
         base08 = "C53253";
