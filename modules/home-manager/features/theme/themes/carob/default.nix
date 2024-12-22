a6bc6b1ddf16a95e4b267afdea59f0a16d8ccc90
yazoink <yazoink@firemail.cc>
2024-12-22 15:40:14 +1100

    tweak

--- modules/home-manager/features/theme/themes/carob/default.nix
+++ modules/home-manager/features/theme/themes/carob/default.nix
@@ -1,7 +1,7 @@
 # depends on system theme module
 {osConfig, lib, ...}:
 {
-  config = lib.mkIf (osConfig.myOptions.desktopTheme.enable && osConfig.myOptions.desktopTheme.name == "caroline") {
+  config = lib.mkIf (osConfig.myOptions.desktopTheme.enable && osConfig.myOptions.desktopTheme.name == "carob") {
     gtk = {
       iconTheme.name = "Carob";
     };
