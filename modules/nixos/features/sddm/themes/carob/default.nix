8a876b030344be8272ac55140964688c07119cc4
yazoink <yazoink@firemail.cc>
2024-12-22 15:30:55 +1100

    tweak

--- modules/nixos/features/sddm/themes/carob/default.nix
+++ modules/nixos/features/sddm/themes/carob/default.nix
@@ -1,6 +1,6 @@
 {config, lib, ...}:
 {
-  config = (lib.mkIf config.myOptions.bundles.desktopBase.sddm.enable  && config.myOptions.desktopTheme.name == "carob") {
+  config = lib.mkIf (config.myOptions.bundles.desktopBase.sddm.enable  && config.myOptions.desktopTheme.name == "carob") {
     services.displayManager.sddm = {
       theme = if (config.networking.hostName == "cyberia") then "carob-small-resolution" else
         (if (config.networking.hostname == "fluoride") then "carob-large-resolution" else "carob");
