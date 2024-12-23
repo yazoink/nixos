9c4dd930c0cd1f1a0316fa91b9544e67b2d50106
yazoink <yazoink@firemail.cc>
2024-12-23 23:03:13 +1100

    tweak

--- modules/nixos/features/theme/themes/caroline/default.nix
+++ modules/nixos/features/theme/themes/caroline/default.nix
@@ -48,8 +48,8 @@ in
         base09 = "A64D3D";
         base0A = "9A8351";
         base0B = "658254";
-        base0C = "34748D";
-        base0D = "424977";
+        base0C = "424977";
+        base0D = "34748D";
         base0E = "876250";
         base0F = "7A5646";
       };
