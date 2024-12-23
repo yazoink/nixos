f8ef06a7ca98276488cab26958929f630992f955
yazoink <yazoink@firemail.cc>
2024-12-23 17:56:20 +1100

    tweak

--- modules/home-manager/features/simple-logout/script.sh
+++ modules/home-manager/features/simple-logout/script.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
+output_dir="/home/$4/.config/simple-logout/icons"
+
+[ -d "$output_dir" ] && rm -rf "$output_dir"
+mkdir -p "$output_dir"
+
 ls_output="$(ls "$2")"
 images=($ls_output)
 
 for image in "${images[@]}"; do
-  $1 "$2/$image" "#A87569" "$3" "/home/$4/.config/simple-logout/icons/$image"
+  $1 "$2/$image" "#A87569" "$3" "$output_dir/$image"
 done
