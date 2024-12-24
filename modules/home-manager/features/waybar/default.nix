15d9db22c6768337aec59642ee055cb2d66801dd
yazoink <yazoink@firemail.cc>
2024-12-24 17:42:23 +1100

    tweak

--- modules/home-manager/features/waybar/default.nix
+++ modules/home-manager/features/waybar/default.nix
@@ -48,7 +48,7 @@
             format = "{index}";
           };
           "clock" = {
-            format = "{:%I:%M %p %d %b %Y}";
+            format = " {:%I:%M %p %d %b %Y}";
             tooltip = false;
             on-click = "gnome-clocks";
           };
