0a3c5570e6664281839eb0331aebfb8229b0b5be
yazoink <yazoink@firemail.cc>
2024-12-24 11:55:33 +1100

    tweak

--- modules/nixos/features/theme/terminal-fonts/default.nix
+++ modules/nixos/features/theme/terminal-fonts/default.nix
@@ -1,7 +1,7 @@
 {config, lib, pkgs, ...}:
 let
-  #customTerminalFont = pkgs.callPackage ./nec-apc-3 {};
-  customTerminalFont = pkgs.callPackage ./jf-dot-kappa-20 {};
+  customTerminalFont = pkgs.callPackage ./nec-apc-3 {};
+  #customTerminalFont = pkgs.callPackage ./jf-dot-kappa-20 {};
 in
 {
   options = {
@@ -14,8 +14,8 @@ in
   };
   config = lib.mkIf config.desktopTheme.customTerminalFont.enable {
     stylix.fonts.monospace = {
-      #name = "Bm437 NEC APC3 8x16";
-      name = "Jf Dot Kappa 20";
+      name = "Bm437 NEC APC3 8x16";
+      #name = "Jf Dot Kappa 20";
       package = customTerminalFont;
     };
   };
