fdccc5b6fb31cec7f804954866d9fc61e424bb4b
yazoink <yazoink@firemail.cc>
2024-12-27 01:04:04 +1100

    auto-generate kvantum theme

--- modules/home-manager/features/theme/kvantum-generator/generate_kvantum.py
+++ modules/home-manager/features/theme/kvantum-generator/generate_kvantum.py
@@ -19,9 +19,9 @@ from os import path, mkdir
 def main():
     new_colors = [
         hex_to_rgb(argv[1]), # bg0
-        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[2]), 0.15), # bg1
-        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[2]), 0.235), # bg2
-        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[5]), 0.115), # bg3
+        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[2]), 0.1), # bg1
+        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[2]), 0.2), # bg2
+        overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[5]), 1.15), # bg3
         overlay_color(hex_to_rgb(argv[1]), hex_to_rgb(argv[2]), 0.8), # bg4
         hex_to_rgb(argv[2]), # fg0
         hex_to_rgb(argv[3]), # fg1
