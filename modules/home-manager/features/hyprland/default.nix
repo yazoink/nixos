1c4dd05f0d28a19a6fb1e7bd5452e95a7ba3a401
yazoink <=>
2024-12-29 16:14:22 +1100

    tweak touchpad options

--- modules/home-manager/features/hyprland/default.nix
+++ modules/home-manager/features/hyprland/default.nix
@@ -79,8 +79,8 @@ in
           {
             name = "etps/2-elantech-touchpad";
             sensitivity = 1.5;
-            accel_profile = "flat";
-            middle_buttons_emulation = true;
+            #accel_profile = "flat";
+            accel_profile = "custom 0.2144477506 0.000 0.307 0.615 1.077 1.539 2.002 2.505 3.208 3.910 4.613 5.315 6.018 6.720 7.423 8.125 8.828 9.530 10.233 10.935 12.387";
           }
         ];
         misc = {
