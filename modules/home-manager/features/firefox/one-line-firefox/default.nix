5fc66a2e814f645361797d5ebb1aba5e8a00dfb7
yazoink <=>
2024-12-30 01:51:03 +1100

    add oneline firefox css

--- modules/home-manager/features/firefox/one-line-firefox/default.nix
+++ modules/home-manager/features/firefox/one-line-firefox/default.nix
@@ -1,6 +1,6 @@
 {osConfig, lib, config, inputs, ...}:
 {
-  config = lib.mkIf (config.bundles.desktopBase.firefox.enable && osConfig.myOptions.desktopTheme.firefoxCss.OneLineFirefox.enable) {
+  config = lib.mkIf (config.bundles.desktopBase.firefox.enable && osConfig.myOptions.desktopTheme.firefoxCss.oneLineFirefox.enable) {
     home.file = {
       ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = ./userChrome.css;
     };
