0fcfa6071636a7aa7a691a4a2fff063e32f2507d
yazoink <=>
2024-12-29 21:20:07 +1100

    remove myOptions.desktopTheme.enable

--- modules/nixos/features/hyprland/default.nix
+++ modules/nixos/features/hyprland/default.nix
@@ -8,7 +8,6 @@
     };
   };
   config = lib.mkIf config.bundles.desktopBase.hyprland.enable {
-    myOptions.desktopTheme.enable = true;
     bundles.desktopBase = {
       gtklock.enable = true;
     };
