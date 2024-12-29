3ffd572acc444e03dfb9be91671bd4d10a72f22b
yazoink <=>
2024-12-29 20:52:50 +1100

    move hyprland env variables

--- modules/home-manager/features/hyprland/default.nix
+++ modules/home-manager/features/hyprland/default.nix
@@ -48,19 +48,17 @@ in
         "$bar" = "/home/${osConfig.myOptions.userAccount.username}/.config/hypr/scripts/waybar.sh";
         "$screenRecorder" = "kooha";
         "$mainMod" = "SUPER";
-        env = {
-          NIXOS_OZONE_WL = "1";
-          MOZ_ENABLE_WAYLAND = "1";
-          GDK_BACKEND = "wayland";
-          #SDL_VIDEO_DRIVER = "wayland"; breaks steam
-          CLUTTER_BACKEND = "wayland";
-          XDG_CURRENT_DESKTOP = "Hyprland";
-          XDG_SESSION_DESKTOP = "Hyprland";
-          XDG_SESSION_TYPE = "wayland";
-          QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-          QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-          QT_QPA_PLATFORM = "wayland";
-        };
+        env = [
+          "NIXOS_OZONE_WL,1"
+          "MOZ_ENABLE_WAYLAND,1"
+          "GDK_BACKEND,wayland"
+          "CLUTTER_BACKEND,wayland"
+          "XDG_CURRENT_DESKTOP,Hyprland"
+          "XDG_SESSION_TYPE,wayland"
+          "QT_AUTO_SCREEN_SCALE_FACTOR,1"
+          "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+          "QT_QPA_PLATFORM,wayland"
+        ];
         exec-once = [
           "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
           "mpDris2 --music-dir=~/mu &"
