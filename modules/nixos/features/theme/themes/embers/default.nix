2fe8e453e74dcfc90e2dd112c0c9cf7c009dd10c
yazoink <yazoink@firemail.cc>
2025-01-01 22:18:20 +1100

    tweak

--- modules/nixos/features/theme/themes/embers/default.nix
+++ modules/nixos/features/theme/themes/embers/default.nix
@@ -1,7 +1,7 @@
 {pkgs, config, lib, ...}:
 {
   config = lib.mkIf (config.myOptions.bundles.desktopBase.enable && config.myOptions.desktopTheme.name == "embers") {
-    desktopTheme.base16Accent = "base09";
+    desktopTheme.base16Accent = "base08";
 
     stylix = {
       base16Scheme = {
