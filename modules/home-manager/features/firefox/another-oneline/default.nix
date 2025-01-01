8133ac6bfd7ec99bc00061d0cdf361548b845bd4
yazoink <yazoink@firemail.cc>
2025-01-01 14:19:15 +1100

    add options for default apps

--- modules/home-manager/features/firefox/another-oneline/default.nix
+++ modules/home-manager/features/firefox/another-oneline/default.nix
@@ -1,6 +1,6 @@
 {osConfig, lib, config, inputs, ...}:
 {
-  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) {
+  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.anotherOneline.enable) {
     home.file = {
       ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = ./userChrome.css;
     };
--- modules/home-manager/features/firefox/shyfox/default.nix
+++ modules/home-manager/features/firefox/shyfox/default.nix
@@ -6,7 +6,7 @@ let
   };
 in
 {
-  config = if (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox") && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) then {
+  config = if (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) then {
     home.file = {
       ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/icons" = {
         source = ./chrome/icons;
