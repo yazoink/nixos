cc45dea71420cfe85623819a249d2a2a1091dc20
yazoink <yazoink@firemail.cc>
2025-01-01 14:16:37 +1100

    add options for default apps

--- modules/home-manager/features/firefox/another-oneline/default.nix
+++ modules/home-manager/features/firefox/another-oneline/default.nix
@@ -1,8 +1,8 @@
 {osConfig, lib, config, inputs, ...}:
 {
-  config = if (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox") && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) then {
+  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) {
     home.file = {
       ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = ./userChrome.css;
     };
-  } else {};
+  };
 }
