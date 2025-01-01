53e68c536550bb5ef781af91706938c4ea5d175f
yazoink <yazoink@firemail.cc>
2025-01-01 18:39:01 +1100

    tweak zen

--- modules/home-manager/features/browsers/zen/default.nix
+++ modules/home-manager/features/browsers/zen/default.nix
@@ -1,6 +1,9 @@
 {inputs, osConfig, lib, ...}:
 {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "zen") {
-    home.packages = [inputs.zen-browser.packages."x86_64-linux".default];
+    programs.firefox = {
+      enable = true;
+      package = [inputs.zen-browser.packages."x86_64-linux".default];
+    };
   };
 }
