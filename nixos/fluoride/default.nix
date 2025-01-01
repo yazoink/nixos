bd5308c8d52035af2897c487ce1bc70468de009b
yazoink <yazoink@firemail.cc>
2025-01-02 00:22:40 +1100

    tweak

--- nixos/fluoride/default.nix
+++ nixos/fluoride/default.nix
@@ -51,7 +51,7 @@
       username = "gene";
     };
     desktopTheme = {
-      name = "spirit";
+      name = "carob";
       wallpaper = ./wallpapers/caroline-flowers.png;
       sddm = {
         scale = 1;
