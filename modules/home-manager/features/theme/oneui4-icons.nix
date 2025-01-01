47ed67eced0f9a9da7b2e01d1fc4395a2a2f8358
yazoink <yazoink@firemail.cc>
2025-01-01 16:58:48 +1100

    trying ghostty

--- modules/home-manager/features/theme/oneui4-icons.nix
+++ modules/home-manager/features/theme/oneui4-icons.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "end-4";
     repo = "OneUI4-Icons";
     rev = "main";
-    sha256 = "";
+    sha256 = "sha256-f5t7VGPmD+CjZyWmhTtuhQjV87hCkKSCBksJzFa1x1Y=";
   };
 
   dontUnpack = true;
