58edf02057579bed45ae5b370ffad8e7bcaf5571
yazoink <yazoink@firemail.cc>
2025-01-01 22:51:48 +1100

    tweak

--- modules/nixos/features/theme/themes/swamp/default.nix
+++ modules/nixos/features/theme/themes/swamp/default.nix
@@ -1,7 +1,7 @@
 {pkgs, config, lib, ...}:
 {
   config = lib.mkIf (config.myOptions.bundles.desktopBase.enable && config.myOptions.desktopTheme.name == "swamp") {
-    desktopTheme.base16Accent = "base0B";
+    desktopTheme.base16Accent = "base0A";
 
     stylix = {
       base16Scheme = {
