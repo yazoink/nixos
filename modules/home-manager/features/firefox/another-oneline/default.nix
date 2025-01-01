1e3524f5724ed7ff0660ff2428f240895d996a73
yazoink <yazoink@firemail.cc>
2025-01-01 14:06:38 +1100

    add options for default apps

--- modules/home-manager/features/firefox/another-oneline/default.nix
+++ modules/home-manager/features/firefox/another-oneline/default.nix
@@ -1,8 +1,8 @@
 {osConfig, lib, config, inputs, ...}:
 {
-  config = (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox") && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) {
+  config = if (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox") && osConfig.myOptions.desktopTheme.firefoxCss.shyfox.enable) then {
     home.file = {
       ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = ./userChrome.css;
     };
-  };
+  } else {};
 }
--- modules/home-manager/features/firefox/default.nix
+++ modules/home-manager/features/firefox/default.nix
@@ -8,7 +8,7 @@
     ./one-line-firefox
   ];
 
-  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox")) {
+  config = if (osConfig.myOptions.bundles.desktopBase.enable && (osConfig.myOptions.defaultApps.webBrowser.command == "firefox")) then {
     home = {
       sessionVariables = {
         MOZ_USE_XINPUT2 = 1;
@@ -88,5 +88,5 @@
         });*/
       };
     };
-  };
+  } else {};
 }
