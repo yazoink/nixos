3ce929d8d2a9a2e5c2d83fe74cb43e50398d5cf7
yazoink <yazoink@firemail.cc>
2025-01-02 18:52:42 +1100

    sway

--- modules/home-manager/features/window-managers/sway/default.nix
+++ modules/home-manager/features/window-managers/sway/default.nix
@@ -47,7 +47,7 @@
         blur disable
         shadows enable
         shadow_blur_radius 10
-        shadow_color #0000007F
+        shadow_color #00000090
         default_dim_inactive 0.2
         layer_effects "waybar" shadows enable;
         layer_effects "rofi" shadows enable;
