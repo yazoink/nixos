7da32ff64f930160a376f20f2b2b27e10f36be93
yazoink <yazoink@firemail.cc>
2025-01-02 17:22:10 +1100

    sway

--- modules/home-manager/features/sway/default.nix
+++ modules/home-manager/features/sway/default.nix
@@ -53,18 +53,18 @@
           background = "#${config.stylix.base16Scheme.base00}";
           focused = {
             #border = "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}";
-            border = "#${config.stylix.base16Scheme.base01}";
-            background = "#${config.stylix.base16Scheme.base01}";
+            border = "#${config.stylix.base16Scheme.base00}";
+            background = "#${config.stylix.base16Scheme.base00}";
             text = "#${config.stylix.base16Scheme.base05}";
-            indicator = "#${config.stylix.base16Scheme.base01}";
-            childBorder = "#${config.stylix.base16Scheme.base01}";
+            indicator = "#${config.stylix.base16Scheme.base00}";
+            childBorder = "#${config.stylix.base16Scheme.base00}";
           };
           focusedInactive = {
-            border = "#${config.stylix.base16Scheme.base01}";
-            background = "#${config.stylix.base16Scheme.base01}";
+            border = "#${config.stylix.base16Scheme.base00}";
+            background = "#${config.stylix.base16Scheme.base00}";
             text = "#${config.stylix.base16Scheme.base04}";
-            indicator = "#${config.stylix.base16Scheme.base01}";
-            childBorder = "#${config.stylix.base16Scheme.base01}";
+            indicator = "#${config.stylix.base16Scheme.base00}";
+            childBorder = "#${config.stylix.base16Scheme.base00}";
           };
           unfocused = {
             border = "#${config.stylix.base16Scheme.base00}";
@@ -75,10 +75,10 @@
           };
           urgent = {
             border = "#${config.stylix.base16Scheme.base01}";
-            background = "#${config.stylix.base16Scheme.base01}";
+            background = "#${config.stylix.base16Scheme.base00}";
             text = "#${config.stylix.base16Scheme.base05}";
-            indicator = "#${config.stylix.base16Scheme.base01}";
-            childBorder = "#${config.stylix.base16Scheme.base01}";
+            indicator = "#${config.stylix.base16Scheme.base00}";
+            childBorder = "#${config.stylix.base16Scheme.base00}";
           };
           placeholder = {
             border = "#${config.stylix.base16Scheme.base00}";
