e6c7f16e0593804e900aaf868f5fb3a2296bf090
yazoink <=>
2025-01-03 19:59:25 +1100

    tweak

--- modules/home-manager/features/theme/themes/rose-pine/default.nix
+++ modules/home-manager/features/theme/themes/rose-pine/default.nix
@@ -1,7 +1,7 @@
 # depends on system theme module
 {osConfig, lib, ...}:
 {
-  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.desktopTheme.name == "rose-pine-moon") {
+  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.desktopTheme.name == "rose-pine") {
     gtk = {
       iconTheme.name = "Rose-Pine";
     };
