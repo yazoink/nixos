b4fa6094ec97ada0646aeb7a4bda843445e25585
yazoink <=>
2025-01-06 00:20:02 +1100

    tweak

--- modules/home-manager/features/theme/themes/biscuit/default.nix
+++ modules/home-manager/features/theme/themes/biscuit/default.nix
@@ -1,7 +1,7 @@
 # depends on system theme module
 {osConfig, lib, ...}:
 {
-  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.desktopTheme.name == "gruvbox-dark-hard") {
+  config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.desktopTheme.name == "biscuit") {
     gtk = {
       iconTheme.name = "papirus-biscuit-dark";
     };
