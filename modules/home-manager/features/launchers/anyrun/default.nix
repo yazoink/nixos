30f3afabb3a572f4cb53d75fa95bb8c9a99d1eea
yazoink <yazoink@firemail.cc>
2025-01-09 14:44:57 +1100

    anyrun

--- modules/home-manager/features/launchers/anyrun/default.nix
+++ modules/home-manager/features/launchers/anyrun/default.nix
@@ -1,4 +1,4 @@
-{config, inputs, lib, osConfig, ...}:
+{config, inputs, lib, pkgs, ...}:
 {
   options = {
     bundles.desktopBase.anyrun.enable = lib.mkOption {
@@ -8,5 +8,19 @@
   };
   config = lib.mkIf config.bundles.desktopBase.anyrun.enable {
     home.packages = [inputs.anyrun.packages."x86_64-linux".anyrun];
+    programs.anyrun = {
+      enable = true;
+      hideIcons = false;
+      ignoreExclusiveZones = false;
+      layer = "overlay";
+      hidePluginInfo = true;
+      closeOnClick = true;
+      maxEntries = null;
+      showResultsImmediately = false;
+      plugins = [
+        inputs.anyrun.packages.${pkgs.system}.applications
+        "${inputs.anyrun.packages.${pkgs.system}.anyrun-with-all-plugins}/lib/kidex"
+      ];
+    };
   };
 }
