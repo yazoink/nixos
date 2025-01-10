782d5801be46db16b9b1da533cc094f1c13f754a
yazoink <=>
2025-01-10 14:18:50 +1100

    tweak

--- modules/home-manager/features/window-managers/sway/hypridle.nix
+++ modules/home-manager/features/window-managers/sway/hypridle.nix
@@ -1,23 +1,26 @@
-{osConfig, lib, ...}:
+{osConfig, lib, config, pkgs, ...}:
+let
+inherit (config.wayland.windowManager.sway) package;
+in
 {
   config = lib.mkIf osConfig.bundles.desktopBase.sway.enable {
     services.hypridle = builtins.trace "hypridle enabled" {
       enable = true;
       settings = {
         general = {
-          after_sleep_cmd = "swaymsg \"output * dpms on\"";
+          after_sleep_cmd = "${package}/bin/swaymsg \"output * dpms on\"";
           ignore_dbus_inhibit = false;
-          lock_cmd = "gtklock";
+          lock_cmd = "${lib.getExe pkgs.gtklock}";
         };
         listener = [
           {
             timeout = 900;
-            on-timeout = "gtklock";
+            on-timeout = "${lib.getExe pkgs.gtklock}";
           }
           {
             timeout = 1200;
-            on-timeout = "swaymsg \"output * dpms off\"";
-            on-resume = "swaymsg \"output * dpms on\"";
+            on-timeout = "${package}/bin/swaymsg \"output * dpms off\"";
+            on-resume = "${package}/bin/swaymsg \"output * dpms on\"";
           }
         ];
       };
