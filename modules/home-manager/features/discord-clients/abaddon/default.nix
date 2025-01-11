857c6616995a4b8338500f0d828d425a2946c0a7
yazoink <=>
2025-01-11 14:34:13 +1100

    abaddon

--- modules/home-manager/features/discord-clients/abaddon/default.nix
+++ modules/home-manager/features/discord-clients/abaddon/default.nix
@@ -1,10 +1,9 @@
 {pkgs, lib, osConfig, ...}:
 let
-  abaddonCss = pkgs.callPackage ./abaddon-css.nix {};
 in
 {
   config = lib.mkIf (osConfig.myOptions.defaultApps.discordClient.command == "abaddon" && osConfig.myOptions.bundles.desktopBase.enable) {
-    home.packages = [pkgs.abaddon abaddonCss];
+    home.packages = [pkgs.abaddon];
 
     systemd.user.services.deploy-abaddon-config = {
       Unit = {
@@ -12,7 +11,7 @@ in
       };
       Service = {
         Type = "oneshot";
-        ExecStart = "${lib.getExe pkgs.bash} ${./script.sh} ${osConfig.sops.secrets.discord_token.path} ${./config.ini} ${osConfig.myOptions.userAccount.username} ${pkgs.coreutils}/bin/cat";
+        ExecStart = "${lib.getExe pkgs.bash} ${./script.sh} ${osConfig.sops.secrets.discord_token.path} ${./config.ini} ${osConfig.myOptions.userAccount.username} ${pkgs.coreutils}/bin/cat ${./style.css}";
       };
       Install = {
         WantedBy = ["default.target"];
--- modules/home-manager/features/discord-clients/abaddon/script.sh
+++ modules/home-manager/features/discord-clients/abaddon/script.sh
@@ -2,8 +2,10 @@ CAT_CMD="$4"
 TOKEN="$($CAT_CMD "$1")"
 CONFIG_TEMPLATE_PATH="$2"
 CONFIG_OUT_PATH="/home/$3/.config/abaddon/abaddon.ini"
+CSS_FILE_PATH="$5"
 
 original_config="$($CAT_CMD "$CONFIG_TEMPLATE_PATH")"
 new_config="${original_config/TOKEN/$TOKEN}"
+new_config="${new_config/CSS_FILE_PATH/$CSS_FILE_PATH}"
 echo "$new_config" > "$CONFIG_OUT_PATH"
 
