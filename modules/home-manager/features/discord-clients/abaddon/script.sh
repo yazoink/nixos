8588814cc43326a66e0ca92a061e716252030e60
yazoink <=>
2025-01-11 14:17:45 +1100

    abaddon

--- modules/home-manager/features/discord-clients/abaddon/script.sh
+++ modules/home-manager/features/discord-clients/abaddon/script.sh
@@ -2,6 +2,10 @@ CAT_CMD="$4"
 TOKEN="$($CAT_CMD "$1")"
 CONFIG_TEMPLATE_PATH="$2"
 CONFIG_OUT_PATH="/home/$3/.config/abaddon/abaddon.ini"
+echo $1
+echo $2
+echo $3
+echo $4
 
 original_config="$($CAT_CMD "$CONFIG_TEMPLATE_PATH")"
 new_config="${original_config/TOKEN/$TOKEN}"
