94d534ff36ee04c9e21e00e136aadcd9ef6f3797
yazoink <=>
2025-01-11 14:07:38 +1100

    abaddon

--- modules/home-manager/features/discord-clients/abaddon/script.sh
+++ modules/home-manager/features/discord-clients/abaddon/script.sh
@@ -1,12 +1,9 @@
-TOKEN="$(cat "$1")"
+TOKEN="$(/run/current-system/sw/bin/cat "$1")"
 CONFIG_TEMPLATE_PATH="$2"
 CONFIG_OUT_PATH="/home/$3/.config/abaddon/abaddon.ini"
 
-main() {
-  [ ! -z "$CONFIG_OUT_PATH" ] && rm -rf "$CONFIG_OUT_PATH"
-  original_config="$(cat "$CONFIG_TEMPLATE_PATH")"
-  new_config="${original_config/TOKEN/$TOKEN}"
-  echo "$new_config" > "$CONFIG_OUT_PATH"
-}
+[ ! -z "$CONFIG_OUT_PATH" ] && rm -rf "$CONFIG_OUT_PATH"
+original_config="$(/run/current-system/sw/bin/cat "$CONFIG_TEMPLATE_PATH")"
+new_config="${original_config/TOKEN/$TOKEN}"
+echo "$new_config" > "$CONFIG_OUT_PATH"
 
-main
