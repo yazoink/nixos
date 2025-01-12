1ba12db9c8c8ba8bd273e657d8f887d95bee4b9c
yazoink <=>
2025-01-12 12:42:10 +1100

    add wallpaper script

--- nixos/stardust/default.nix
+++ nixos/stardust/default.nix
@@ -15,7 +15,7 @@
   myOptions = {
     userAccount.username = "gene";
     desktopTheme = {
-      name = "levuaska";
+      name = "everblush";
       #name = "gruvbox-material-dark-hard";
       #wallpaper = ./wallpapers/caroline-flowers3.png;
       #wallpaper = ./wallpapers/painting3.jpg;
