b22f871ded0d910f574b72e11bd0068dd2dceecd
yazoink <yazoink@firemail.cc>
2025-01-13 17:45:04 +1100

    fix cd thing maybe

--- modules/home-manager/features/disk-burner/default.nix
+++ modules/home-manager/features/disk-burner/default.nix
@@ -3,6 +3,8 @@
   config = lib.mkIf osConfig.myOptions.hardwareFeatures.diskBurner.enable {
     home.packages = with pkgs; [
       brasero
+      cdrdao
+      cdrtools
       dvdplusrwtools
       gst_all_1.gstreamer
       gst_all_1.gst-plugins-base
