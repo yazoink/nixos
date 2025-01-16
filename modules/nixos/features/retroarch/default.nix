e02830bf93b9e09ef686b494fd9996b161aa9ee2
yazoink <yazoink@firemail.cc>
2025-01-16 14:49:48 +1100

    update

--- modules/nixos/features/retroarch/default.nix
+++ modules/nixos/features/retroarch/default.nix
@@ -2,19 +2,7 @@
 {
   config = lib.mkIf config.myOptions.features.retroarch.enable {
     environment.systemPackages = with pkgs; [
-      (retroarch.withCores (libretro: with libretro; [
-        snes9x
-        beetle-psx-hw
-        beetle-gba
-        desmume
-        pcsx2
-        mupen64plus
-        dolphin
-        flycast
-        ppsspp
-        nestopia
-        easyrpg
-      ]))
+      retroarch-full
     ];
   };
 }
