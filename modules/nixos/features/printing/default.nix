f75630534f06d4ba920f38b5ec0d3178bbcc1771
yazoink <yazoink@firemail.cc>
2025-01-16 18:01:19 +1100

    update

--- modules/nixos/features/printing/default.nix
+++ modules/nixos/features/printing/default.nix
@@ -10,7 +10,7 @@
     bundles.base.avahi.enable = true;
     services.printing = {
       enable = true;
-      drivers = with pkgs; [gutenprint gutenprintBin];
+      drivers = with pkgs; [gutenprint gutenprintBin fxlinuxprint];
     };
   };
 }
