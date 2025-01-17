aea23d393ac4997b60d562215bd71254003fedd8
yazoink <=>
2025-01-17 13:58:59 +1100

    tweak sway media keys

--- modules/home-manager/features/window-managers/sway/default.nix
+++ modules/home-manager/features/window-managers/sway/default.nix
@@ -219,8 +219,8 @@
           #"${modifier}+r" = "exec ${lib.getExe pkgs.kooha}";
           "XF86MonBrightnessDown" = "exec ${lib.getExe pkgs.light} -U 10";
           "XF86MonBrightnessUp" = "exec ${lib.getExe pkgs.light} -A 10";
-          "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +1%'";
-          "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -1%'";
+          "XF86AudioRaiseVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ +5%'";
+          "XF86AudioLowerVolume" = "exec 'pactl set-sink-volume @DEFAULT_SINK@ -5%'";
           "XF86AudioMute" = "exec 'pactl set-sink-mute @DEFAULT_SINK@ toggle'";
         };
       };
