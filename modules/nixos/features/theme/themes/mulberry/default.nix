68534a6a1c6696230e603dd1641b9a0352ecc180
yazoink <=>
2025-01-19 14:37:21 +1100

    add colorscheme

--- modules/nixos/features/theme/themes/mulberry/default.nix
+++ modules/nixos/features/theme/themes/mulberry/default.nix
@@ -6,22 +6,22 @@
 
     stylix = {
       base16Scheme = {
-        base00 = "#0F0F0F";
-        base01 = "#1B1B1B";
-        base02 = "#272627";
-        base03 = "#3E3C3E";
-        base04 = "#857E84";
-        base05 = "#CAC0C9";
-        base06 = "#CFC6CE";
-        base07 = "#D3CBD2";
-        base08 = "#3B6363";
-        base09 = "#BA9BAA";
-        base0A = "#A13A57";
-        base0B = "#62545D";
-        base0C = "#A9758A";
-        base0D = "#995C6C";
-        base0E = "#747D8B";
-        base0F = "#876D7C";
+        base00 = "0F0F0F";
+        base01 = "1B1B1B";
+        base02 = "272627";
+        base03 = "3E3C3E";
+        base04 = "857E84";
+        base05 = "CAC0C9";
+        base06 = "CFC6CE";
+        base07 = "D3CBD2";
+        base08 = "3B6363";
+        base09 = "BA9BAA";
+        base0A = "A13A57";
+        base0B = "62545D";
+        base0C = "A9758A";
+        base0D = "995C6C";
+        base0E = "747D8B";
+        base0F = "876D7C";
       };
 
     /*stylix = {
