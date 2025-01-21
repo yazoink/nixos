a27e2fa34e03f86dde4a4f2759dca49f115f8b17
yazoink <=>
2025-01-21 13:51:48 +1100

    pywalfox for stylix

--- modules/home-manager/features/browsers/firefox/default.nix
+++ modules/home-manager/features/browsers/firefox/default.nix
@@ -13,6 +13,32 @@
       sessionVariables = {
         MOZ_USE_XINPUT2 = 1;
       };
+      file = {
+        ".cache/wal/colors.json".text = with config.stylix.base16Scheme; ''
+          {
+            "wallpaper": "${config.stylix.image}",
+            "alpha": "100",
+            "colors": {
+              "color0": "#${base00}",
+              "color1": "",
+              "color2": "",
+              "color3": "",
+              "color4": "",
+              "color5": "",
+              "color6": "",
+              "color7": "",
+              "color8": "",
+              "color9": "",
+              "color10": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}",
+              "color11": "",
+              "color12": "",
+              "color13": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}",
+              "color14": "",
+              "color15": "${base00}"
+            }
+          }
+        '';
+      };
     };
 
     programs.firefox = {
@@ -74,7 +100,8 @@
           #foxytab
           libredirect
           floccus
-          firefox-color
+          #firefox-color
+          pywalfox
           control-panel-for-twitter
         ] ++ (
             if osConfig.myOptions.hardwareFeatures.h264ify.enable 
