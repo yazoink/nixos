0331bdc78f4ca11788d02672ba12f61368328a1e
yazoink <yazoink@firemail.cc>
2025-01-25 15:44:23 +1100

    change theme

--- nixos/fluoride/default.nix
+++ nixos/fluoride/default.nix
@@ -51,12 +51,12 @@
       username = "gene";
     };
     desktopTheme = {
-      name = "caroline";
-      wallpaper = ./wallpapers/caroline-flowers2.png;
+      name = "paradise";
+      wallpaper = ./wallpapers/painting1.jpg;
       terminalPadding = 24;
       sddm = {
         scale = 0.8;
-        wallpaper = ./wallpapers/caroline-flowers3.png;
+        wallpaper = ./wallpapers/painting1.jpg;
       };
       firefoxCss.anotherOneline.enable = true;
     };
