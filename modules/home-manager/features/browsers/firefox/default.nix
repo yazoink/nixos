f06f54dc9c61eb695635f4b810a4002fe370f1f2
yazoink <yazoink@firemail.cc>
2025-01-25 19:59:26 +1100

    theme

--- modules/home-manager/features/browsers/firefox/default.nix
+++ modules/home-manager/features/browsers/firefox/default.nix
@@ -1,10 +1,16 @@
 # https://color.firefox.com/?theme=XQAAAAL_AgAAAAAAAABBKYhm849SCicxcUN7ViuG_ebZUZXOFqnmjhZm1VFQCfj9bl943WDccz_Akz8SdUz8TFDGjl_ZSYYSICc2Zbsikn4WogycX4Aie2e8UIx8D0-R1bJlWOnsaWYvvowcHXh2ADAwxi27AXVZyrKfixs7vGPbYXqVtT4SaXQv9ggsolbZNrStW3mxj6bVYfAAnSi3t5-i-43_qmD59hj83e3pcFhlZN5GBTm6_ERzrdCap3Aoog7iGO-nb4dUL1ay7q9-9rTNqKruawiWDvdR813n6v_PeVKseRW4sIC6j0rwvUs_fopDoztHJi5FdjnDgOMzD_It2eC81zKm_6vshk0omkie3-a6kFULzbqWzErFq81W96ldlvO6do-X2YLYsyTzWqTPXqZxX3XU0FkIl4kUwEBn_XlfO4f0uUOxpsJ0t7L8NY6m1AvjGjROm_9S138A
-{inputs, config, lib, osConfig, pkgs, ...}:
 {
+  inputs,
+  config,
+  lib,
+  osConfig,
+  pkgs,
+  ...
+}: {
   imports = [
-    ./shyfox 
-    ./firefox-ui-fix 
-    ./another-oneline 
+    ./shyfox
+    ./firefox-ui-fix
+    ./another-oneline
     ./one-line-firefox
   ];
 
@@ -15,30 +21,31 @@
       };
       packages = with pkgs; [pywalfox-native];
       file = {
-        ".cache/wal/colors.json".text = with config.stylix.base16Scheme;  builtins.trace "remember to run pywalfox install and set background light to 1, background extra to 2 and accent secondary to 3. Under palette template, set toolbar field highlight and popup highlight to accent secondary" ''
-          {
-            "wallpaper": "${config.stylix.image}",
-            "alpha": "100",
-            "colors": {
-              "color0": "#${base00}",
-              "color1": "#${base01}",
-              "color2": "#${base02}",
-              "color3": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}",
-              "color4": "",
-              "color5": "",
-              "color6": "",
-              "color7": "#${base05}",
-              "color8": "",
-              "color9": "",
-              "color10": "#${base05}",
-              "color11": "",
-              "color12": "",
-              "color13": "#${base05}",
-              "color14": "",
-              "color15": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}"
+        ".cache/wal/colors.json".text = with config.stylix.base16Scheme;
+          builtins.trace "remember to run pywalfox install and set background light to 1, background extra to 2 and accent secondary to 3. Under palette template, set toolbar field highlight and popup highlight to accent secondary" ''
+            {
+              "wallpaper": "${config.stylix.image}",
+              "alpha": "100",
+              "colors": {
+                "color0": "#${base00}",
+                "color1": "#${base01}",
+                "color2": "#${base02}",
+                "color3": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}",
+                "color4": "",
+                "color5": "",
+                "color6": "",
+                "color7": "#${base05}",
+                "color8": "",
+                "color9": "",
+                "color10": "#${base05}",
+                "color11": "",
+                "color12": "",
+                "color13": "#${base05}",
+                "color14": "",
+                "color15": "#${config.stylix.base16Scheme.${osConfig.desktopTheme.base16Accent}}"
+              }
             }
-          }
-        '';
+          '';
       };
     };
 
@@ -48,7 +55,7 @@
         DisableTelemetry = true;
         DisableFirefoxStudies = true;
         EnableTrackingProtection = {
-          Value= true;
+          Value = true;
           Locked = true;
           Cryptomining = true;
           Fingerprinting = true;
@@ -84,36 +91,42 @@
           privateDefault = "4get";
           engines = {
             "4get" = {
-              urls = [{
-                template = "https://4get.yazo.ink/web?s={searchTerms}";
-              }];
-              definedAliases = [ "@4g" ];
+              urls = [
+                {
+                  template = "https://4get.yazo.ink/web?s={searchTerms}";
+                }
+              ];
+              definedAliases = ["@4g"];
             };
           };
         };
-        extensions = with inputs.firefox-addons.packages."x86_64-linux"; [
-          ublock-origin
-          sponsorblock
-          stylus
-          violentmonkey
-          localcdn
-          clearurls
-          #foxytab
-          libredirect
-          floccus
-          #firefox-color
-          pywalfox
-          control-panel-for-twitter
-        ] ++ (
-            if osConfig.myOptions.hardwareFeatures.h264ify.enable 
-            then [enhanced-h264ify] 
+        extensions = with inputs.firefox-addons.packages."x86_64-linux";
+          [
+            ublock-origin
+            sponsorblock
+            stylus
+            violentmonkey
+            localcdn
+            clearurls
+            #foxytab
+            libredirect
+            floccus
+            #firefox-color
+            pywalfox
+            control-panel-for-twitter
+          ]
+          ++ (
+            if osConfig.myOptions.hardwareFeatures.h264ify.enable
+            then [enhanced-h264ify]
             else []
           );
         extraConfig = builtins.readFile ./user.js;
-        /*extraConfig = builtins.readFile (builtins.fetchurl {
+        /*
+          extraConfig = builtins.readFile (builtins.fetchurl {
           url = "https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js";
           sha256 = "0vfn3s7q4gm939a5cim1jlrr8k0f5pm4kxxsv1d9bwvv10pmz7pn";
-        });*/
+        });
+        */
       };
     };
   };
