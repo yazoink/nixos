ac21c92af8a0a927df9b1348427992dabe29a30a
yazoink <yazoink@firemail.cc>
2025-01-27 03:02:32 +1100

    tweak

--- nixos/cyberia/default.nix
+++ nixos/cyberia/default.nix
@@ -57,11 +57,11 @@
         size = 11;
       };
       #wallpaper = ./wallpapers/levuaska/clouds5.jpg;
-      wallpaper = ./wallpapers/painting3.jpg;
+      wallpaper = ./wallpapers/painting2.jpg;
       sddm = {
         scale = 1.3;
         #wallpaper = ./wallpapers/levuaska/flowers.jpg;
-        wallpaper = ./wallpapers/painting3.jpg;
+        wallpaper = ./wallpapers/painting2.jpg;
       };
       firefoxCss.stylix.enable = true;
     };
