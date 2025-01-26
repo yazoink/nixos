0f365065205af6147e8aab15da9cc41edc471582
yazoink <=>
2025-01-26 15:02:08 +1100

    add cursors

--- modules/home-manager/features/window-managers/sway/default.nix
+++ modules/home-manager/features/window-managers/sway/default.nix
@@ -1,5 +1,10 @@
-{config, lib, pkgs, osConfig, ...}:
 {
+  config,
+  lib,
+  pkgs,
+  osConfig,
+  ...
+}: {
   imports = [./hypridle.nix];
   config = lib.mkIf osConfig.bundles.desktopBase.sway.enable {
     bundles.desktopBase = {
@@ -39,7 +44,7 @@
       XCURSOR_SIZE = config.stylix.cursor.size;
       _JAVA_AWT_WM_NONREPARENTING = 1;
     };
-    
+
     stylix.targets.sway.enable = false;
 
     wayland.windowManager.sway = {
@@ -47,106 +52,110 @@
       package = pkgs.swayfx;
       xwayland = true;
       checkConfig = false;
-      extraConfig = ''
-        focus_follows_mouse yes
-        title_align center
-        titlebar_padding 10
-        titlebar_border_thickness 0
-        corner_radius 15
-        #smart_corner_radius on
-        default_border normal 0
-        blur disable
-        shadows enable
-        shadow_blur_radius 15
-        shadow_color #000000AA
-        #default_dim_inactive 0.15
-        titlebar_separator disable
-        layer_effects "waybar" shadows enable;
-        #layer_effects "rofi" shadows enable;
-        #layer_effects "gtk-layer-shell" shadows enable;
-        #layer_effects "notifications" shadows enable;
+      extraConfig =
+        ''
+          focus_follows_mouse yes
+          title_align center
+          titlebar_padding 14
+          titlebar_border_thickness 0
+          corner_radius 15
+          #smart_corner_radius on
+          default_border normal 0
+          blur disable
+          shadows enable
+          shadow_blur_radius 15
+          shadow_color #000000AA
+          #default_dim_inactive 0.15
+          titlebar_separator disable
+          layer_effects "waybar" shadows enable;
+          #layer_effects "rofi" shadows enable;
+          #layer_effects "gtk-layer-shell" shadows enable;
+          #layer_effects "notifications" shadows enable;
 
-        output * scale 1
+          output * scale 1
 
-        bindsym --whole-window {
-          ${config.wayland.windowManager.sway.config.modifier}+button4 workspace prev
-          ${config.wayland.windowManager.sway.config.modifier}+button5 workspace next
-        }
+          bindsym --whole-window {
+            ${config.wayland.windowManager.sway.config.modifier}+button4 workspace prev
+            ${config.wayland.windowManager.sway.config.modifier}+button5 workspace next
+          }
 
-        for_window [class="Firefox"] inhibit_idle fullscreen
-        for_window [app_id="firefox"] inhibit_idle fullscreen
+          for_window [class="Firefox"] inhibit_idle fullscreen
+          for_window [app_id="firefox"] inhibit_idle fullscreen
 
-        input type:touchpad {
-          dwt disabled
-          tap enabled
-        }
+          input type:touchpad {
+            dwt disabled
+            tap enabled
+          }
 
-        bindgesture swipe:right workspace next
-        bindgesture swipe:left workspace prev
+          bindgesture swipe:right workspace next
+          bindgesture swipe:left workspace prev
 
-        for_window {
-          [title="^File Upload.*"] floating enable
-          [title="^Open File.*"] floating enable
-          [title="^Open Folder.*"] floating enable
-          [title="^Save As.*"] floating enable
-          [title="^Select a File.*"] floating enable
-          [title="^Picture-in-Picture$"] floating enable, sticky enable
-          [app_id="firefox" title="Picture-in-Picture"] floating enable, sticky enable
-          [title="^galculator$"] floating enable
-          [app_id="galculator"] floating enable
-          [title="^Empty Trash$"] floating enable
-          [title="^File Manager Preferences$"] floating enable
-          [class="^org.kde.kruler$"] floating enable
-          [instance="Browser" window_role="About"] floating enable
-          [title = "About Mozilla Firefox"] floating enable
-          [window_role = "About"] floating enable
-          [window_role="pop-up"] floating enable
-          [window_role="bubble"] floating enable
-          [window_role="dialog"] floating enable
-          [window_type="dialog"] floating enable
-          [app_id="floating"] floating enable
-          [title=".* is sharing your screen."] floating enable
-          [app_id="firefox" title="Firefox — Sharing Indicator"] floating enable
-          [app_id="zenity"] floating enable
-          [app_id="^chrome-.*__.*$"] shortcuts_inhibitor disable
-        }
+          for_window {
+            [title="^File Upload.*"] floating enable
+            [title="^Open File.*"] floating enable
+            [title="^Open Folder.*"] floating enable
+            [title="^Save As.*"] floating enable
+            [title="^Select a File.*"] floating enable
+            [title="^Picture-in-Picture$"] floating enable, sticky enable
+            [app_id="firefox" title="Picture-in-Picture"] floating enable, sticky enable
+            [title="^galculator$"] floating enable
+            [app_id="galculator"] floating enable
+            [title="^Empty Trash$"] floating enable
+            [title="^File Manager Preferences$"] floating enable
+            [class="^org.kde.kruler$"] floating enable
+            [instance="Browser" window_role="About"] floating enable
+            [title = "About Mozilla Firefox"] floating enable
+            [window_role = "About"] floating enable
+            [window_role="pop-up"] floating enable
+            [window_role="bubble"] floating enable
+            [window_role="dialog"] floating enable
+            [window_type="dialog"] floating enable
+            [app_id="floating"] floating enable
+            [title=".* is sharing your screen."] floating enable
+            [app_id="firefox" title="Firefox — Sharing Indicator"] floating enable
+            [app_id="zenity"] floating enable
+            [app_id="^chrome-.*__.*$"] shortcuts_inhibitor disable
+          }
 
-        set $handle_dialog floating enable, move container to workspace current
-        for_window {
-            [app_id="soffice" title="Document Recovery$"] $handle_dialog
-            [app_id="chromium" title="Open File"] $handle_dialog
-            [app_id="firefox" title="^Enter name of file to save to…$"] $handle_dialog
-            [app_id="firefox" title="^Open File$"] $handle_dialog
-            [app_id="firefox" title="^Save As$"] $handle_dialog
-            [app_id="firefox" title="^Save Image$"] $handle_dialog
-            [app_id="firefox" title="File Upload"] $handle_dialog
-            [app_id="firefox" title="^Firefox - Choose User Profile$"] $handle_dialog
-            [app_id="firefox" title="^Developer Tools — "] $handle_dialog
-            [app_id="firefox" title="^Opening "] $handle_dialog
-            [app_id="firefox" title="^Extension: \(MetaMask\) - MetaMask Notification — Mozilla Firefox$"] $handle_dialog
-            [app_id="Chromium" title="^DevTools - "] $handle_dialog
-            [app_id="chromium-bin-browser" title="^Save File$"] $handle_dialog
-            [app_id="telegram-desktop" title="^Choose Files$"] $handle_dialog
-            [app_id="firefox" title="^Choose Application$"] $handle_dialog
-            [title="(?:Open|Save) (?:File|Folder|As)"] $handle_dialog
-            [app_id="pop-up,task_dialog,About"] $handle_dialog;
-            [app_id="zenity"] $handle_dialog
-            [window_role="pop-up,task_dialog,About"] $handle_dialog;
-            [window_type="dialog"] $handle_dialog;
-            [window_type="utility"] $handle_dialog;
-            [window_type="toolbar"] $handle_dialog;
-            [window_type="splash"] $handle_dialog;
-            [window_type="menu"] $handle_dialog;
-            [window_type="dropdown_menu"] $handle_dialog;
-            [window_type="popup_menu"] $handle_dialog;
-            [window_type="tooltip"] $handle_dialog;
-            [window_type="notification"] $handle_dialog;
-        }
+          set $handle_dialog floating enable, move container to workspace current
+          for_window {
+              [app_id="soffice" title="Document Recovery$"] $handle_dialog
+              [app_id="chromium" title="Open File"] $handle_dialog
+              [app_id="firefox" title="^Enter name of file to save to…$"] $handle_dialog
+              [app_id="firefox" title="^Open File$"] $handle_dialog
+              [app_id="firefox" title="^Save As$"] $handle_dialog
+              [app_id="firefox" title="^Save Image$"] $handle_dialog
+              [app_id="firefox" title="File Upload"] $handle_dialog
+              [app_id="firefox" title="^Firefox - Choose User Profile$"] $handle_dialog
+              [app_id="firefox" title="^Developer Tools — "] $handle_dialog
+              [app_id="firefox" title="^Opening "] $handle_dialog
+              [app_id="firefox" title="^Extension: \(MetaMask\) - MetaMask Notification — Mozilla Firefox$"] $handle_dialog
+              [app_id="Chromium" title="^DevTools - "] $handle_dialog
+              [app_id="chromium-bin-browser" title="^Save File$"] $handle_dialog
+              [app_id="telegram-desktop" title="^Choose Files$"] $handle_dialog
+              [app_id="firefox" title="^Choose Application$"] $handle_dialog
+              [title="(?:Open|Save) (?:File|Folder|As)"] $handle_dialog
+              [app_id="pop-up,task_dialog,About"] $handle_dialog;
+              [app_id="zenity"] $handle_dialog
+              [window_role="pop-up,task_dialog,About"] $handle_dialog;
+              [window_type="dialog"] $handle_dialog;
+              [window_type="utility"] $handle_dialog;
+              [window_type="toolbar"] $handle_dialog;
+              [window_type="splash"] $handle_dialog;
+              [window_type="menu"] $handle_dialog;
+              [window_type="dropdown_menu"] $handle_dialog;
+              [window_type="popup_menu"] $handle_dialog;
+              [window_type="tooltip"] $handle_dialog;
+              [window_type="notification"] $handle_dialog;
+          }
 
-        seat * xcursor_theme "${config.stylix.cursor.name}" ${builtins.toString config.stylix.cursor.size}
-      '' + (if (osConfig.myOptions.defaultApps.terminal.command == "footclient") 
-        then "exec ${pkgs.foot}/bin/foot --server" 
-        else "");
+          seat * xcursor_theme "${config.stylix.cursor.name}" ${builtins.toString config.stylix.cursor.size}
+        ''
+        + (
+          if (osConfig.myOptions.defaultApps.terminal.command == "footclient")
+          then "exec ${pkgs.foot}/bin/foot --server"
+          else ""
+        );
       config = rec {
         modifier = "Mod4";
         floating.modifier = modifier;
@@ -169,7 +178,7 @@
         fonts = {
           names = [config.stylix.fonts.sansSerif.name "Font Awesome 6 Free"];
           style = "Bold";
-          size  = "${builtins.toString config.stylix.fonts.sizes.desktop}";
+          size = "${builtins.toString config.stylix.fonts.sizes.desktop}";
         };
         colors = {
           background = "#${config.stylix.base16Scheme.base00}";
