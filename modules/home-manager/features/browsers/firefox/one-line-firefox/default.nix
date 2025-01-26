7f0e909abd35693245a3af698593106dcaec43b1
yazoink <yazoink@firemail.cc>
2025-01-26 22:08:25 +1100

    tweak

--- modules/home-manager/features/browsers/firefox/one-line-firefox/default.nix
+++ modules/home-manager/features/browsers/firefox/one-line-firefox/default.nix
@@ -1,13 +1,26 @@
-{osConfig, lib, config, inputs, ...}:
 {
-  config = if (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.oneLineFirefox.enable) then {
-    home.file = {
-      ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = ./userChrome.css;
-    };
-    programs.firefox.profiles."${osConfig.myOptions.userAccount.username}".settings = {
-      "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-      "browser.compactmode.show" = true;
-      "extensions.pocket.enabled" = false;
-    };
-  } else {};
+  osConfig,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  userChromeFile = config.lib.stylix.colors {
+    template = ./userChrome.css.mustache;
+    extension = ".css";
+  };
+in {
+  config =
+    if (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.webBrowser.command == "firefox" && osConfig.myOptions.desktopTheme.firefoxCss.oneLineFirefox.enable)
+    then {
+      home.file = {
+        ".mozilla/firefox/${osConfig.myOptions.userAccount.username}/chrome/userChrome.css".source = userChromeFile;
+      };
+      programs.firefox.profiles."${osConfig.myOptions.userAccount.username}".settings = {
+        "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.compactmode.show" = true;
+        "extensions.pocket.enabled" = false;
+      };
+    }
+    else {};
 }
