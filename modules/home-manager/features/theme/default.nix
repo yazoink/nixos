fa21a339e396f3dfffcc7fff12017357936bf0b4
yazoink <yazoink@firemail.cc>
2025-02-06 11:55:53 +1100

    tweak

--- modules/home-manager/features/theme/default.nix
+++ modules/home-manager/features/theme/default.nix
@@ -73,46 +73,46 @@ in {
       };
     };
 
-    # home = {
-    #   file = {
-    #     ".config/qt5ct/qt5ct.conf".text = ''
-    #       [Appearance]
-    #       custom_palette=false
-    #       style=kvantum
-    #       icon_theme=${config.gtk.iconTheme.name}
-    #       standard_dialogs=default
-    #
-    #       [Fonts]
-    #       fixed="${config.stylix.fonts.monospace.name},${toString config.stylix.fonts.sizes.applications},-1,5,5,50,0,0,0,0,Regular"
-    #       general="${config.stylix.fonts.sansSerif.name},${toString config.stylix.fonts.sizes.applications},-1,5,50,0,0,0,0,0,Regular"
-    #
-    #       [Interface]
-    #       activate_item_on_single_click=1
-    #       buttonbox_layout=0
-    #       cursor_flash_time=1000
-    #       dialog_buttons_have_icons=1
-    #       double_click_interval=400
-    #       gui_effects=@Invalid()
-    #       keyboard_scheme=2
-    #       menus_have_icons=true
-    #       show_shortcuts_in_context_menus=true
-    #       stylesheets=@Invalid()
-    #       toolbutton_style=4
-    #       underline_shortcut=1
-    #       wheel_scroll_lines=3
-    #
-    #       [SettingsWindow]
-    #       geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\0\0\0\0\0\0\0\x2\xb7\0\0\x3\x46\0\0\0\0\0\0\0\0\0\0\x2\xde\0\0\x3\x43\0\0\0\0\x2\0\0\0\x6@\0\0\0\0\0\0\0\0\0\0\x2\xb7\0\0\x3\x46)
-    #
-    #       [Troubleshooting]
-    #       force_raster_widgets=1
-    #       ignored_applications=@Invalid()
-    #     '';
-    #     ".config/Kvantum/kvantum.kvconfig".text = ''
-    #       [General]
-    #       theme=KvLibadwaitaRecolor
-    #     '';
-    #   };
+    home = {
+      #   file = {
+      #     ".config/qt5ct/qt5ct.conf".text = ''
+      #       [Appearance]
+      #       custom_palette=false
+      #       style=kvantum
+      #       icon_theme=${config.gtk.iconTheme.name}
+      #       standard_dialogs=default
+      #
+      #       [Fonts]
+      #       fixed="${config.stylix.fonts.monospace.name},${toString config.stylix.fonts.sizes.applications},-1,5,5,50,0,0,0,0,Regular"
+      #       general="${config.stylix.fonts.sansSerif.name},${toString config.stylix.fonts.sizes.applications},-1,5,50,0,0,0,0,0,Regular"
+      #
+      #       [Interface]
+      #       activate_item_on_single_click=1
+      #       buttonbox_layout=0
+      #       cursor_flash_time=1000
+      #       dialog_buttons_have_icons=1
+      #       double_click_interval=400
+      #       gui_effects=@Invalid()
+      #       keyboard_scheme=2
+      #       menus_have_icons=true
+      #       show_shortcuts_in_context_menus=true
+      #       stylesheets=@Invalid()
+      #       toolbutton_style=4
+      #       underline_shortcut=1
+      #       wheel_scroll_lines=3
+      #
+      #       [SettingsWindow]
+      #       geometry=@ByteArray(\x1\xd9\xd0\xcb\0\x3\0\0\0\0\0\0\0\0\0\0\0\0\x2\xb7\0\0\x3\x46\0\0\0\0\0\0\0\0\0\0\x2\xde\0\0\x3\x43\0\0\0\0\x2\0\0\0\x6@\0\0\0\0\0\0\0\0\0\0\x2\xb7\0\0\x3\x46)
+      #
+      #       [Troubleshooting]
+      #       force_raster_widgets=1
+      #       ignored_applications=@Invalid()
+      #     '';
+      #     ".config/Kvantum/kvantum.kvconfig".text = ''
+      #       [General]
+      #       theme=KvLibadwaitaRecolor
+      #     '';
+      #   };
 
       sessionVariables = {
         #FREETYPE_PROPERTIES = "cff:no-stem-darkening=0 autofitter:no-stem-darkening=0"; # make gtk font rendering match qt
