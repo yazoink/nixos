9c2791c049485fe9180c32dc4b78ce6e376cc86c
yazoink <=>
2025-02-20 09:51:03 +1100

    fix thing

--- modules/home-manager/features/browsers/firefox/default.nix
+++ modules/home-manager/features/browsers/firefox/default.nix
@@ -102,7 +102,7 @@
             };
           };
         };
-        extensions = with inputs.firefox-addons.packages."x86_64-linux";
+        extensions.packages = with inputs.firefox-addons.packages."x86_64-linux";
           [
             ublock-origin
             sponsorblock
