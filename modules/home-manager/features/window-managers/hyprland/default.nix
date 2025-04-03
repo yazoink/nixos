b9d86e99ebef9fcd05017f75c0aa0a49c3c5a306
yazoink <=>
2025-04-03 14:34:25 +1100

    thing

--- modules/home-manager/features/window-managers/hyprland/default.nix
+++ modules/home-manager/features/window-managers/hyprland/default.nix
@@ -20,6 +20,11 @@ in {
     ./hypridle.nix
   ];
   config = lib.mkIf osConfig.bundles.desktopBase.hyprland.enable {
+    nix.settings = {
+      substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    };
+
     stylix.targets.hyprland.enable = false;
 
     bundles.desktopBase = {
