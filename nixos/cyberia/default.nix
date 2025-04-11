a8816ffd88f5ca34ca164faf8ab19ee3b4b9f04b
yazoink <yazoink@firemail.cc>
2025-04-11 16:12:25 +1000

    tweak

--- nixos/cyberia/default.nix
+++ nixos/cyberia/default.nix
@@ -33,7 +33,7 @@
         # name = "SFMono";
         # name = "Bm437 NEC APC3 8x16";
         # name = "Terminus";
-        size = 11;
+        size = 12;
       };
       fonts.desktop = {
         # name = "SF Pro Display";
