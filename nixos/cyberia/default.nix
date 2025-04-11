f99270a8425ad5dbd036b5e504396c40f4d74c7f
yazoink <yazoink@firemail.cc>
2025-04-11 15:59:36 +1000

    tweak

--- nixos/cyberia/default.nix
+++ nixos/cyberia/default.nix
@@ -31,7 +31,7 @@
       fonts.terminal = {
         # name = "SFMono";
         # name = "Bm437 NEC APC3 8x16";
-        name = "Cozette";
+        name = "Terminus";
         size = 12;
       };
       fonts.desktop = {
@@ -42,7 +42,7 @@
       # wallpaper = ./wallpapers/clouds6.jpg;
       wallpaper = {
         # image = ./wallpapers/wall1.png;
-        image = ./wallpapers/flowers-1.jpg;
+        image = ./wallpapers/abstract1.jpg;
         solidColor = {
           enable = false;
           hex = config.stylix.base16Scheme.base03;
@@ -51,7 +51,7 @@
       sddm = {
         scale = 1.3;
         # wallpaper = ./wallpapers/clouds6.jpg;
-        wallpaper = ./wallpapers/flowers-1.jpg;
+        wallpaper = ./wallpapers/abstract1.jpg;
       };
       firefoxCss.stylix.enable = true;
     };
