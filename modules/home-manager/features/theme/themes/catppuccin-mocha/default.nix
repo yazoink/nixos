fde8ba98e6bc53d738f0d49826dd66d5f849d27a
yazoink <yazoink@firemail.cc>
2025-04-21 09:04:59 +1000

    tweak

--- modules/home-manager/features/theme/themes/catppuccin-mocha/default.nix
+++ modules/home-manager/features/theme/themes/catppuccin-mocha/default.nix
@@ -7,7 +7,7 @@
 }: {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.desktopTheme.name == "catppuccin-mocha") {
     stylix = {
-      iconTheme.dark = "Papirus-dark";
+      iconTheme.dark = "Papirus";
       iconTheme.package = pkgs.catppuccin-papirus-folders;
     };
   };
