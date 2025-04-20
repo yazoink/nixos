043bc338146ae91f9fbd4a9b9744439ebca915ba
yazoink <yazoink@firemail.cc>
2025-04-21 07:19:24 +1000

    tweak

--- nixos/cyberia/default.nix
+++ nixos/cyberia/default.nix
@@ -27,7 +27,7 @@
     };
     desktopTheme = {
       # name = "paradise";
-      name = "moonfly";
+      name = "mountain";
       fonts.terminal = {
         name = "Iosevka Nerd Font";
         # name = "SFMono";
