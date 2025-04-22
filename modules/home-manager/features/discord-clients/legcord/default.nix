723476be188e666e62d14ab219d645ad83afc7e5
yazoink <yazoink@firemail.cc>
2025-04-22 19:15:49 +1000

    add legcord

--- modules/home-manager/features/discord-clients/legcord/default.nix
+++ modules/home-manager/features/discord-clients/legcord/default.nix
@@ -15,8 +15,8 @@
 in {
   config = lib.mkIf (osConfig.myOptions.defaultApps.discordClient.command == "legcord" && osConfig.myOptions.bundles.desktopFull.enable) {
     xdg.configFile = {
-      "legcord/themes/my.theme.css".source = themeFile;
-      "legcord/themes/fonts.theme.css".text = ''
+      "legcord/userAssets/my.theme.css".source = themeFile;
+      "legcord/userAssets/fonts.theme.css".text = ''
         :root {
           --font: ${config.stylix.fonts.sansSerif.name};
           --font-primary: ${config.stylix.fonts.sansSerif.name};
