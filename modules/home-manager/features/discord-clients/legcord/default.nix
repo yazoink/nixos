b86d824f684e2a4c6a3a01471facdc3f19e8b4a0
yazoink <yazoink@firemail.cc>
2025-04-22 19:09:18 +1000

    add legcord

---
+++ modules/home-manager/features/discord-clients/legcord/default.nix
@@ -0,0 +1,39 @@
+{
+  pkgs,
+  config,
+  lib,
+  osConfig,
+  ...
+}: let
+  themeFile = config.lib.stylix.colors {
+    template =
+      if (osConfig.myOptions.desktopTheme.name == "catppuccin-mocha")
+      then ./catppuccin.theme.css.mustache
+      else ./my.theme.css.mustache;
+    extension = ".css";
+  };
+in {
+  config = lib.mkIf (osConfig.myOptions.defaultApps.discordClient.command == "legcord" && osConfig.myOptions.bundles.desktopFull.enable) {
+    xdg.configFile = {
+      "vesktop/themes/my.theme.css".source = themeFile;
+      "vesktop/themes/fonts.theme.css".text = ''
+        :root {
+          --font: ${config.stylix.fonts.sansSerif.name};
+          --font-primary: ${config.stylix.fonts.sansSerif.name};
+          --font-display: ${config.stylix.fonts.sansSerif.name};
+        }
+
+        * {
+          font-family: ${config.stylix.fonts.sansSerif.name};
+        }
+
+        body {
+          --font: ${config.stylix.fonts.sansSerif.name};
+          --font-primary: ${config.stylix.fonts.sansSerif.name};
+          --font-display: ${config.stylix.fonts.sansSerif.name};
+        }
+      '';
+    };
+    home.packages = with pkgs; [legcord];
+  };
+}
