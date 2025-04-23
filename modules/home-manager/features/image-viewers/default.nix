a0b91a9deb0208ed81e9e2ca27a8f40e465000a2
yazoink <yazoink@firemail.cc>
2025-04-23 16:28:27 +1000

    tweak

--- modules/home-manager/features/image-viewers/default.nix
+++ modules/home-manager/features/image-viewers/default.nix
@@ -1,5 +1,10 @@
-{
+{lib, ...}: {
   imports = [
     ./ristretto
   ];
+  options = {
+    defaultApps.imageViewer.desktopFile = lib.mkOption {
+      type = lib.types.str;
+    };
+  };
 }
--- modules/home-manager/features/image-viewers/ristretto/default.nix
+++ modules/home-manager/features/image-viewers/ristretto/default.nix
@@ -1,6 +1,11 @@
-{pkgs, lib, osConfig, ...}:
 {
+  pkgs,
+  lib,
+  osConfig,
+  ...
+}: {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.imageViewer.command == "ristretto") {
+    defaultApps.imageViewer.desktopFile = "ristretto.desktop";
     home.packages = [pkgs.xfce.ristretto];
   };
 }
