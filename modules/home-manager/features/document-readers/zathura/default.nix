f0368741c5ee86410c8c94199c075767ea09f3aa
yazoink <yazoink@firemail.cc>
2025-04-23 17:31:55 +1000

    add zathura

--- modules/home-manager/features/document-readers/zathura/default.nix
+++ modules/home-manager/features/document-readers/zathura/default.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.documentReader.command == "zathura") {
-    defaultApps.documentReader.desktopFile = "zathura.desktop";
+    defaultApps.documentReader.desktopFile = "org.pwmt.zathura.desktop";
     programs.zathura = {
       enable = true;
     };
