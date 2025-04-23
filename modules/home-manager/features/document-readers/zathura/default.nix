0d8c07c9c5fb8edfea1a657aee7a10fa04ad8a8b
yazoink <yazoink@firemail.cc>
2025-04-23 17:37:11 +1000

    add zathura

--- modules/home-manager/features/document-readers/zathura/default.nix
+++ modules/home-manager/features/document-readers/zathura/default.nix
@@ -1,12 +1,16 @@
 {
   lib,
   osConfig,
+  config,
   ...
 }: {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.enable && osConfig.myOptions.defaultApps.documentReader.command == "zathura") {
     defaultApps.documentReader.desktopFile = "org.pwmt.zathura.desktop";
     programs.zathura = {
       enable = true;
+      extraConfig = ''
+        set font "${config.stylix.fonts.sansSerif.name} ${builtins.toString config.stylix.fonts.sizes.applications}"
+      '';
     };
   };
 }
