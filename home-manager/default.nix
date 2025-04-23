af7c2f52bb5aea9cba1a45d2f02b99df9aa0d172
yazoink <yazoink@firemail.cc>
2025-04-23 15:38:28 +1000

    tweak

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,18 +1,13 @@
 {
   inputs,
-  outputs,
-  lib,
-  config,
-  pkgs,
+  osConfig,
   ...
-}: {
+}: let
+  inherit (osConfig.myOptions.userAccount) username;
+in {
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
     inputs.nixcord.homeManagerModules.nixcord
-    ./cyberia
-    ./fluoride
-    ./stardust
-    ./tallulah
     ../modules/home-manager
   ];
 
@@ -23,7 +18,19 @@
   };
 
   programs.home-manager.enable = builtins.trace "home-manager enabled" true;
+  home = {
+    username = "${username}";
+    homeDirectory = "/home/${username}";
+    sessionVariables = {
+      EDITOR = "nvim";
+      BROWSER = "${osConfig.myOptions.defaultApps.webBrowser.command}";
+      STEAM_EXTRA_COMPAT_TOOLS_PATH = "\${HOME}/.steam/root/compatibilitytools.d";
+    };
+  };
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "24.05";
 }
