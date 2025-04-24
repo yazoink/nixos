9e660121f26bfa60a6d7d417b0b1ce6ffc39ac43
yazoink <yazoink@firemail.cc>
2025-04-24 18:39:40 +1000

    tweak

--- modules/nixos/features/user/default.nix
+++ modules/nixos/features/user/default.nix
@@ -1,5 +1,9 @@
-{config, lib, pkgs, ...}:
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   options = {
     bundles.base.user.enable = lib.mkOption {
       type = lib.types.bool;
@@ -8,6 +12,7 @@
   };
   config = lib.mkIf config.bundles.base.user.enable {
     programs.zsh.enable = true;
+    security.sudo.wheelNeedsPassword = false;
     users = {
       defaultUserShell = pkgs.zsh;
       users."${config.myOptions.userAccount.username}" = {
@@ -17,20 +22,44 @@
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGazraEEc7mlqlzLB4BePXgNG5tkb0UiHWBuC29PyJUN gmav@nixserver1"
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPUe+C8GS6Xvtz2r+ArNzf0cvgiSlod6nnLVhgDZUuV gmav@nixserver2"
         ];
-        extraGroups = [
-          "wheel"
-          "cdrom"
-        ] ++ (if config.programs.adb.enable then ["adbusers"] else [])
-          ++ (if config.myOptions.bundles.desktopBase.enable then [
-            "audio"
-            "video"
-          ] else [])
-          ++ (if config.myOptions.features.virtManager.enable then ["libvirtd"] else [])
-          ++ (if config.networking.networkmanager.enable then ["networkmanager"] else [])
-          ++ (if config.programs.gamemode.enable then ["gamemode"] else []);
+        extraGroups =
+          [
+            "wheel"
+            "cdrom"
+          ]
+          ++ (
+            if config.programs.adb.enable
+            then ["adbusers"]
+            else []
+          )
+          ++ (
+            if config.myOptions.bundles.desktopBase.enable
+            then [
+              "audio"
+              "video"
+            ]
+            else []
+          )
+          ++ (
+            if config.myOptions.features.virtManager.enable
+            then ["libvirtd"]
+            else []
+          )
+          ++ (
+            if config.networking.networkmanager.enable
+            then ["networkmanager"]
+            else []
+          )
+          ++ (
+            if config.programs.gamemode.enable
+            then ["gamemode"]
+            else []
+          );
       };
       extraGroups.vboxusers.members =
-        if config.virtualisation.virtualbox.host.enable then ["${config.myOptions.userAccount.username}"] else [];
+        if config.virtualisation.virtualbox.host.enable
+        then ["${config.myOptions.userAccount.username}"]
+        else [];
     };
   };
 }
