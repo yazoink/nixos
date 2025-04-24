d2b548c02cc451128ab7900f5139e6d39a3d1130
yazoink <yazoink@firemail.cc>
2025-04-24 10:19:50 +1000

    tweak

--- modules/nixos/features/window-managers/hyprland/default.nix
+++ modules/nixos/features/window-managers/hyprland/default.nix
@@ -48,8 +48,10 @@
     programs.hyprland = {
       enable = true;
       xwayland.enable = true;
-      package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-      portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      # package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+      # portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+      package = null;
+      portalPackage = null;
       withUWSM = false;
     };
 
