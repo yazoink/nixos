2340c34ca17bc21f1580f65b692fbb7db5e1cfdb
yazoink <yazoink@firemail.cc>
2025-04-24 12:14:01 +1000

    tweak

--- modules/home-manager/features/tmux/default.nix
+++ modules/home-manager/features/tmux/default.nix
@@ -1,5 +1,8 @@
-{config, lib, ...}:
 {
+  config,
+  lib,
+  ...
+}: {
   options = {
     bundles.base.tmux.enable = lib.mkOption {
       type = lib.types.bool;
@@ -11,26 +14,6 @@
       enable = true;
       terminal = "screen-256color";
       clock24 = false;
-      extraConfig = ''
-        # remap prefix from 'C-b' to 'C-a'
-        unbind C-b
-        set-option -g prefix C-a
-
-        # split panes using | and -
-        bind | split-window -h
-        bind - split-window -v
-        unbind '"'
-        unbind %
-
-        switch panes using Alt-arrow without prefix
-        bind -n M-Left select-pane -L
-        bind -n M-Right select-pane -R
-        bind -n M-Up select-pane -U
-        bind -n M-Down select-pane -D
-
-        # Enable mouse control (clickable windows, panes, resizable panes)
-        set -g mouse on
-      '';
     };
   };
 }
