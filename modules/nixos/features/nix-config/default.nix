07f7888fdb90826b388abcc8d69dfb1d9a8127fd
yazoink <yazoink@firemail.cc>
2025-05-05 14:45:47 +1000

    tweak

--- modules/nixos/features/nix-config/default.nix
+++ modules/nixos/features/nix-config/default.nix
@@ -37,6 +37,7 @@
     };
 
     hardware.enableAllFirmware = true;
+    hardware.enableRedistributableFirmware = true;
 
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     system.stateVersion = "24.05";
