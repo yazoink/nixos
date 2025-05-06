cded8c4c6cb515d54433a0443b15e069858bd023
yazoink <yazoink@firemail.cc>
2025-05-06 21:59:11 +1000

    tweak

--- modules/home-manager/features/swayosd/default.nix
+++ modules/home-manager/features/swayosd/default.nix
@@ -1,12 +1,14 @@
 {
   config,
   lib,
+  osConfig,
   ...
 }: let
-  style = config.lib.stylix.colors {
-    template = ./style.css.mustache;
-    extension = ".css";
-  };
+  inherit (config.stylix) base16Scheme;
+  # style = config.lib.stylix.colors {
+  #   template = ./style.css.mustache;
+  #   extension = ".css";
+  # };
 in {
   options = {
     bundles.desktopBase.swayosd.enable = lib.mkOption {
@@ -18,6 +20,22 @@ in {
     services.swayosd = {
       enable = true;
     };
-    xdg.configFile."swayosd/style.css".source = style;
+    # xdg.configFile."swayosd/style.css".source = style;
+    xdg.configFile."swayosd/style.css".text = ''
+      window#osd {
+        padding: 0px;
+        border-radius: 999px;
+        background-color: #${base16Scheme.base00};
+        border: 1px solid #${base16Scheme.base01};
+      }
+
+      window#osd #container {
+        color: #${base16Scheme.base05};
+      }
+
+      window#osd progress {
+        background-color: #${base16Scheme."${osConfig.desktopTheme.base16Accent}"};
+      }
+    '';
   };
 }
