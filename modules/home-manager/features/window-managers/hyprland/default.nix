e440840bf1fddcc7cf050e1b7b399571ee672a5b
yazoink <yazoink@firemail.cc>
2025-05-06 21:53:35 +1000

    tweak

--- modules/home-manager/features/window-managers/hyprland/default.nix
+++ modules/home-manager/features/window-managers/hyprland/default.nix
@@ -404,6 +404,9 @@ in {
           "$mainMod, mouse:272, movewindow"
           "$mainMod, mouse:273, resizewindow"
         ];
+        bindr = [
+          "CAPS, Caps_Lock, exec, swayosd-client --caps-lock"
+        ];
         windowrulev2 = [
           "plugin:hyprbars:title_color rgb(${config.stylix.base16Scheme.base05}), focus:1"
           "suppressevent maximize, class:.*"
