d45a508388f9aae8a43588809c4ff2ea1af2860e
yazoink <yazoink@firemail.cc>
2025-05-06 19:37:58 +1000

    tweak

--- modules/home-manager/features/ironbar/default.nix
+++ modules/home-manager/features/ironbar/default.nix
@@ -228,7 +228,7 @@ in {
                     "type": "button",
                     "class": "search-icon",
                     "label": "",
-                    "on_click": "!wofi --show drun --location=1 --xoffset=5 --yoffset=5",
+                    "on_click": "!wofi --show drun --location=1 --xoffset=5 --yoffset=5 --prompt 'Start searching or press Esc to exit'",
                     "tooltip": "Open application launcher"
                   }
                 ]
