389b5ed15c6098daa8d67a714a0b3d07b43cdc27
yazoink <=>
2025-05-08 18:20:43 +1000

    tweak

--- modules/home-manager/features/ironbar/scripts/battery_icon.sh
+++ modules/home-manager/features/ironbar/scripts/battery_icon.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-status="$(cat /sys/class/power_supply/"$1"/status)"
+# Get battery status and capacity in one go to minimize file reads
+status_file="/sys/class/power_supply/$1/status"
+capacity_file="/sys/class/power_supply/$1/capacity"
 
-[[ "$status" == "Charging" ]] && {
-    echo " "
+# Read both values efficiently
+read -r status < "$status_file"
+read -r battery < "$capacity_file"
+
+# If charging, print the icon and exit
+if [[ $status == "Charging" ]]; then
+    echo ""
     exit 0
-}
-
-battery=$(cat /sys/class/power_supply/"$1"/capacity)
-
-if [[ $battery -lt 21 ]]; then
-    echo " "
-elif [[ $battery -lt 41 ]]; then
-    echo " "
-elif [[ $battery -lt 61 ]]; then
-    echo " "
-elif [[ $battery -lt 81 ]]; then
-    echo " "
-else
-    echo " "
 fi
+
+# Determine battery icon based on capacity
+case $battery in
+    [0-1][0-9]|20)   icon="" ;;  # 0-20
+    2[1-9]|3[0-9]|40) icon="" ;; # 21-40
+    4[1-9]|5[0-9]|60) icon="" ;; # 41-60
+    6[1-9]|7[0-9]|80) icon="" ;; # 61-80
+    *)                icon="" ;; # 81-100+
+esac
+
+echo "$icon"
