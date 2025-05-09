e92df90e99a084eacdd6512c22bdd7a34e16f06c
yazoink <=>
2025-05-09 16:40:12 +1000

    restructure firefox config

--- modules/home-manager/features/web-browsers/firefox/default.nix
+++ modules/home-manager/features/web-browsers/firefox/default.nix
@@ -8,18 +8,20 @@
   inherit (osConfig.myOptions) bundles defaultApps;
 in {
   imports = [./theme];
-  config = lib.mkIf (bundles.desktopBase.enable && defaultApps.webBrowser.command == "firefox") lib.mkMerge [
-    {
-      home = {
-        sessionVariables = {
-          MOZ_USE_XINPUT2 = 1;
+  config = lib.mkIf (bundles.desktopBase.enable && defaultApps.webBrowser.command == "firefox") (
+    lib.mkMerge [
+      {
+        home = {
+          sessionVariables = {
+            MOZ_USE_XINPUT2 = 1;
+          };
         };
-      };
-      programs.firefox = lib.mkMerge [
-        {enable = true;}
-        (import ./config {inherit lib osConfig inputs;})
-      ];
-    }
-    (import ./theme {inherit config osConfig;})
-  ];
+        programs.firefox = lib.mkMerge [
+          {enable = true;}
+          (import ./config {inherit lib osConfig inputs;})
+        ];
+      }
+      (import ./theme {inherit config osConfig;})
+    ]
+  );
 }
