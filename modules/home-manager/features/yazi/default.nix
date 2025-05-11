6ce43232afdfc2ee644ec272202e9ad624dbb17d
yazoink <yazoink@firemail.cc>
2025-05-11 14:16:26 +1000

    tweak

--- modules/home-manager/features/yazi/default.nix
+++ modules/home-manager/features/yazi/default.nix
@@ -27,77 +27,138 @@ in {
         };
         icon = {
           rules = [];
-        };
-        theme = {
-          filetype = {
-            rules = lib.mkForce [
-              {
-                fg = "#${base16Scheme.base0C}";
-                mime = "image/*";
-              }
-              {
-                fg = "#${base16Scheme.base0A}";
-                mime = "video/*";
-              }
-              {
-                fg = "#${base16Scheme.base0A}";
-                mime = "audio/*";
-              }
+          theme = with config.lib.stylix.colors.withHashtag; let
+            mkFg = fg: {inherit fg;};
+            mkBg = bg: {inherit bg;};
+            mkBoth = fg: bg: {inherit fg bg;};
+            mkSame = c: (mkBoth c c);
+          in {
+            manager = rec {
+              # Reusing bat themes, since it's suggested in the stying guide
+              # https://yazi-rs.github.io/docs/configuration/theme#manager
+              syntect_theme = config.lib.stylix.colors {
+                template = ./base16-stylix.tmTheme.mustache;
+                extension = ".tmTheme";
+              };
 
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/zip";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/gzip";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/tar";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/bzip";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/bzip2";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/7z-compressed";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/rar";
-              }
-              {
-                fg = "#${base16Scheme.base0E}";
-                mime = "application/xz";
-              }
+              cwd = mkFg cyan;
+              hovered =
+                (mkBoth base05 base02)
+                // {
+                  bold = true;
+                };
+              preview_hovered = hovered;
+              find_keyword =
+                (mkFg green)
+                // {
+                  bold = true;
+                };
+              find_position = mkFg magenta;
+              marker_selected = mkSame yellow;
+              marker_copied = mkSame green;
+              marker_cut = mkSame red;
+              tab_active = mkBoth base00 blue;
+              tab_inactive = mkBoth base05 base01;
+              border_style = mkFg base04;
+            };
 
-              {
-                fg = "#${base16Scheme.base0B}";
-                mime = "application/doc";
-              }
-              {
-                fg = "#${base16Scheme.base0B}";
-                mime = "application/pdf";
-              }
-              {
-                fg = "#${base16Scheme.base0B}";
-                mime = "application/rtf";
-              }
-              {
-                fg = "#${base16Scheme.base0B}";
-                mime = "application/vnd.*";
-              }
+            mode = {
+              normal_main =
+                (mkBoth base00 blue)
+                // {
+                  bold = true;
+                };
+              normal_alt = mkBoth blue base00;
+              select_main =
+                (mkBoth base00 green)
+                // {
+                  bold = true;
+                };
+              select_alt = mkBoth green base00;
+              unset_main =
+                (mkBoth base00 brown)
+                // {
+                  bold = true;
+                };
+              unset_alt = mkBoth brown base00;
+            };
 
-              {
-                fg = accent;
-                mime = "inode/directory";
-              }
+            status = {
+              progress_label = mkBoth base05 base00;
+              progress_normal = mkBoth base05 base00;
+              progress_error = mkBoth red base00;
+              perm_type = mkFg blue;
+              perm_read = mkFg yellow;
+              perm_write = mkFg red;
+              perm_exec = mkFg green;
+              perm_sep = mkFg cyan;
+            };
+
+            pick = {
+              border = mkFg blue;
+              active = mkFg magenta;
+              inactive = mkFg base05;
+            };
+
+            input = {
+              border = mkFg blue;
+              title = mkFg base05;
+              value = mkFg base05;
+              selected = mkBg base03;
+            };
+
+            completion = {
+              border = mkFg blue;
+              active = mkBoth magenta base03;
+              inactive = mkFg base05;
+            };
+
+            tasks = {
+              border = mkFg blue;
+              title = mkFg base05;
+              hovered = mkBoth base05 base03;
+            };
+
+            which = {
+              mask = mkBg base02;
+              cand = mkFg cyan;
+              rest = mkFg brown;
+              desc = mkFg base05;
+              separator_style = mkFg base04;
+            };
+
+            help = {
+              on = mkFg magenta;
+              run = mkFg cyan;
+              desc = mkFg base05;
+              hovered = mkBoth base05 base03;
+              footer = mkFg base05;
+            };
+
+            # https://github.com/sxyazi/yazi/blob/main/yazi-config/preset/theme.toml
+            filetype.rules = let
+              mkRule = mime: fg: {inherit mime fg;};
+            in [
+              (mkRule "image/*" cyan)
+              (mkRule "video/*" yellow)
+              (mkRule "audio/*" yellow)
+
+              (mkRule "application/zip" magenta)
+              (mkRule "application/gzip" magenta)
+              (mkRule "application/tar" magenta)
+              (mkRule "application/bzip" magenta)
+              (mkRule "application/bzip2" magenta)
+              (mkRule "application/7z-compressed" magenta)
+              (mkRule "application/rar" magenta)
+              (mkRule "application/xz" magenta)
+
+              (mkRule "application/doc" green)
+              (mkRule "application/pdf" green)
+              (mkRule "application/rtf" green)
+              (mkRule "application/vnd.*" green)
+
+              ((mkRule "inode/directory" config.lib.stylix.colors.${osConfig.desktopTheme.base16Accent}) // {bold = true;})
+              (mkRule "*" base05)
             ];
           };
         };
