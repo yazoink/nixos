dfd5a2621ac5c9a95fe8b0280f20feb1b5f14800
yazoink <yazoink@firemail.cc>
2025-05-11 15:49:48 +1000

    add firefox gnome theme with tweaks

--- modules/home-manager/features/web-browsers/firefox/theme/default.nix
+++ modules/home-manager/features/web-browsers/firefox/theme/default.nix
@@ -9,6 +9,7 @@
     extension = ".css";
   };
   inherit (osConfig.myOptions.userAccount) username;
+  inherit (config.lib.stylix.colors);
 in {
   stylix.targets.firefox = {
     enable = true;
@@ -20,11 +21,11 @@ in {
       @import "${config.stylix.inputs.firefox-gnome-theme}/userChrome.css";
       @import "${userChrome}";
       :root {
-        --gnome-entry-focused-border-color: color-mix(in-srgb, #${config.lib.stylix.colors.${osConfig.desktopTheme.base16Accent}}, 30%, black);
+        --gnome-entry-focused-border-color: color-mix(in-srgb, #${config.lib.stylix.colors.${osConfig.desktopTheme.base16Accent}} 30%, black);
       }
       #urlbar-input::selection {
         color: #${config.lib.stylix.colors.base00} !important;
-        background-color: color-mix(in srgb, #${config.lib.stylix.colors.${osConfig.desktopTheme.base16Accent}}, 40%, black) !important;
+        background-color: color-mix(in srgb, #${config.lib.stylix.colors.${osConfig.desktopTheme.base16Accent}} 40%, black) !important;
       }
       menupopup {
         border: 1px solid #${config.lib.stylix.colors.base01};
