57f9e23d96ccc69b88c492e11f9fe0fcdbf1796f
yazoink <yazoink@firemail.cc>
2025-05-22 11:30:24 +1000

    tweak

--- modules/home-manager/features/mpdris2-rs/mpdris2-rs.nix
+++ modules/home-manager/features/mpdris2-rs/mpdris2-rs.nix
@@ -9,5 +9,5 @@ pkgs.rustPlatform.buildRustPackage rec {
     sha256 = "sha256-ro5k6zYg36NJoyGkuH2dh8KNUWt+J/eFuyst/r8Jr3E=";
   };
   useFetchCargoVendor = true;
-  cargoHash = "sha256-R9aatiWr8O6nVeRzHRgtD1rzG0e6JCUzVhryFz7iuZg=";
+  cargoHash = "sha256-tB7ATjNTwnuwrWwFshuZWjcJsvcBaV7/I59pNhRNabg=";
 }
