77e5ab545adeb5e88c54db7188fdc1cd489b46bd
yazoink <yazoink@firemail.cc>
2025-07-15 18:27:30 +1000

    thing

--- modules/home-manager/features/mpdris2-rs/mpdris2-rs.nix
+++ modules/home-manager/features/mpdris2-rs/mpdris2-rs.nix
@@ -6,7 +6,7 @@ pkgs.rustPlatform.buildRustPackage rec {
     owner = "szclsya";
     repo = "mpdris2-rs";
     rev = "trunk";
-    sha256 = "sha256-zIyVlv1LL+uJq8/nfzj9xOyHN22srQpPlUgxkRDnNUg=";
+    sha256 = "sha256-NSFd1dhXqcYOR9361TFRko/HTPIJ8Go3nyBeIgEpnV8=";
   };
   useFetchCargoVendor = true;
   cargoHash = "sha256-AhzSA26KgFCL03W0+TOTSy0e5t4owA3TUM/+mbI6dmg=";
