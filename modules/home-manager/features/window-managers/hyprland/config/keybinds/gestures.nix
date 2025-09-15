4f06a0d861842752efeca16744c5a006f4fc45cd
yazoink <yazoink@firemail.cc>
2025-09-15 13:02:39 +1000

    fix thing

--- modules/home-manager/features/window-managers/hyprland/config/keybinds/gestures.nix
+++ modules/home-manager/features/window-managers/hyprland/config/keybinds/gestures.nix
@@ -1,10 +1,8 @@
 {
-  gestures = {
-    # workspace_swipe = 1;
-    workspace_swipe_touch = true;
-    # workspace_swipe_fingers = 3;
-    workspace_swipe_forever = true;
-    workspace_swipe_direction_lock = false;
-    workspace_swipe_create_new = true;
-  };
+  gesture = [
+    "3, horizontal, workspace"
+    "3, vertical, close"
+    "3, pinchin, fullscreen, maximize"
+    "3, pinchout, float, tile"
+  ];
 }
