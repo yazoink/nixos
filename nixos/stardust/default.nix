28318cd2da2e30fcb827af919de8483518dbc7bf
yazoink <yazoink@firemail.cc>
2025-10-03 13:36:01 +1000

    add factorio

--- nixos/stardust/default.nix
+++ nixos/stardust/default.nix
@@ -13,6 +13,8 @@
     inputs.stylix.nixosModules.stylix
   ];
 
+  environment.systemPackages = with pkgs; [factorio]; # move later
+
   home-manager = {
     extraSpecialArgs = {inherit inputs;};
     useGlobalPkgs = true;
