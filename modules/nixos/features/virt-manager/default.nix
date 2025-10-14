c4eeb600ed41df60802b9e67e78a5019fe62aaee
yazoink <yazoink@firemail.cc>
2025-10-15 08:45:49 +1100

    fix

--- modules/nixos/features/virt-manager/default.nix
+++ modules/nixos/features/virt-manager/default.nix
@@ -17,15 +17,6 @@
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMFFull.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
-        };
       };
     };
 
