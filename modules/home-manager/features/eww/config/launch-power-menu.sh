8a33853f5861d520bb0fb824cf4f957f567c55c1
yazoink <yazoink@firemail.cc>
2025-10-22 16:36:18 +1100

    tweak

--- modules/home-manager/features/eww/config/launch-power-menu.sh
+++ modules/home-manager/features/eww/config/launch-power-menu.sh
@@ -9,7 +9,7 @@ if [[ $? != 0 ]]; then
         hyprctl keyword bindn ,S,exec,systemctl shutdown
         hyprctl keyword bindn ,Escape,exec,eww close power
         while true; do
-            sleep 0.2
+            sleep 0.5
             eww active-windows | grep "power"
             if [[ $? != 0 ]]; then
                 echo "RELOADING HYPRLAND"
