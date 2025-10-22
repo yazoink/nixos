37b1a6af9900b93d5bf5a91216319af94269c850
yazoink <yazoink@firemail.cc>
2025-10-23 10:32:03 +1100

    tweak

--- modules/home-manager/features/no-display-manager/default.nix
+++ modules/home-manager/features/no-display-manager/default.nix
@@ -2,12 +2,17 @@
   osConfig,
   lib,
   ...
-}: {
+}: let
+  wm =
+    if (osConfig.bundles.desktopBase.windowManager == "hyprland")
+    then "Hyprland"
+    else "sway";
+in {
   config = lib.mkIf (osConfig.myOptions.bundles.desktopBase.displayManager == "none" && osConfig.myOptions.bundles.desktopBase.enable) {
     home.file.".zprofile" = {
       text = ''
         if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then
-          exec Hyprland
+          exec ${wm}
         fi
       '';
       executable = true;
