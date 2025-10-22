64a20f464810bb8f7cfa5cca53edf08f33f5673a
yazoink <yazoink@firemail.cc>
2025-10-22 22:59:27 +1100

    tweak

--- modules/home-manager/features/window-managers/scripts/waybar.sh
+++ modules/home-manager/features/window-managers/scripts/waybar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-bash "~/.config/widgets/launch-corners.sh"
+bash ~/.config/widgets/launch-corners.sh
 pidof waybar && pkill waybar
 echo "---" | tee -a /tmp/waybar.log
 waybar 2>&1 | tee -a /tmp/waybar.log & disown
