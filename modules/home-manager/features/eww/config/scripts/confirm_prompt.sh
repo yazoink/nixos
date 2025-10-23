341ad1406c838eb6064aeeeeb382dc310a2bd403
yazoink <yazoink@firemail.cc>
2025-10-23 18:10:13 +1100

    tweak

--- modules/home-manager/features/eww/config/scripts/confirm_prompt.sh
+++ modules/home-manager/features/eww/config/scripts/confirm_prompt.sh
@@ -9,16 +9,14 @@ eww --config "$config" open confirm --arg screen=$monitor --arg thing="$1" --arg
 if [[ $? == 0 ]]; then
     echo "confirm prompt opened"
     sleep 0.6
-    hyprctl keyword bindn ,Y,exec,"$2; eww --config $config close confirm"
-    hyprctl keyword bindn ,N,exec,"eww --config $config close confirm"
-    hyprctl keyword bindn ,"Escape,exec,eww --config $config close confirm"
+    hyprctl keyword bindn ,Y,exec,"hyprctl reload; $2; eww --config $config close confirm"
+    hyprctl keyword bindn ,N,exec,"hyprctl reload; eww --config $config close confirm"
+    hyprctl keyword bindn ,"Escape,exec,hyprctl reload; eww --config $config close confirm"
     echo "set keybinds"
     while true; do
         sleep 0.5
         eww --config "$config" active-windows | grep -q "confirm"
         if [[ $? != 0 ]]; then
-            hyprctl reload
-            echo "hyprland reloaded"
             exit 0
         fi
     done
