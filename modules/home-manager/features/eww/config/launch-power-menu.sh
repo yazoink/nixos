69ef6e7480a9552bc4353a8d1747931a8ff0e9ce
yazoink <yazoink@firemail.cc>
2025-10-23 18:11:51 +1100

    tweak

--- modules/home-manager/features/eww/config/launch-power-menu.sh
+++ modules/home-manager/features/eww/config/launch-power-menu.sh
@@ -22,18 +22,16 @@ echo "pwd $(pwd)"
 if [[ $? == 0 ]]; then
     echo "power menu opened"
     hyprctl keyword bindn ,L,exec,hyprlock
-    hyprctl keyword bindn ,E,exec,"EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to exit to TTY?' 'hyprctl dispatch exit'"
-    hyprctl keyword bindn ,R,exec,"EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to reboot?' 'systemctl reboot'"
-    hyprctl keyword bindn ,S,exec,"EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to shut down?' 'systemctl shutdown'"
-    hyprctl keyword bindn ,Escape,exec,"EWW_CONFIG_DIR=$config eww --config $config close power"
+    hyprctl keyword bindn ,E,exec,"hyprctl reload; EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to exit to TTY?' 'hyprctl dispatch exit'"
+    hyprctl keyword bindn ,R,exec,"hyprctl reload; EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to reboot?' 'systemctl reboot'"
+    hyprctl keyword bindn ,S,exec,"hyprctl reload; EWW_CONFIG_DIR=$config bash $config/scripts/confirm_prompt.sh 'Are you sure you want to shut down?' 'systemctl shutdown'"
+    hyprctl keyword bindn ,Escape,exec,"hyprctl reload; EWW_CONFIG_DIR=$config eww --config $config close power"
     echo "set keybinds"
     while true; do
         sleep 0.5
         echo "looping"
         eww --config "$config" active-windows | grep -q "power"
         if [[ $? -ne 0 ]]; then
-            hyprctl reload
-            echo "hyprland reloaded"
             exit 0
         fi
     done
