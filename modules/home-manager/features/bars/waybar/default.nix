b3349cea4574a262c8cb211046a9d753d3773330
yazoink <yazoink@firemail.cc>
2025-11-18 19:44:35 +1100

    tweak

--- modules/home-manager/features/bars/waybar/default.nix
+++ modules/home-manager/features/bars/waybar/default.nix
@@ -9,7 +9,7 @@
   inherit (osConfig.myOptions.hardwareFeatures) laptop;
   inherit (osConfig.myOptions) desktopTheme;
   iconSize = desktopTheme.fonts.desktop.size - 1;
-  barHeight = 42;
+  barHeight = 46;
   barPosition = "bottom";
   workspacesModule = ''
     "hyprland/workspaces": {
