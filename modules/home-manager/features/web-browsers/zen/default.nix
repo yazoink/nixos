4ed6233b85e2b53f5a78df52bf1358fe8ebf5913
yazoink <yazoink@firemail.cc>
2025-11-19 12:26:21 +1100

    tweak

--- modules/home-manager/features/web-browsers/zen/default.nix
+++ modules/home-manager/features/web-browsers/zen/default.nix
@@ -60,6 +60,7 @@
           "zen.view.compact.animate-sidebar" = false;
           "zen.welcome-screen.seen" = true;
           "zen.urlbar.behavior" = "float";
+
           "browser.aboutConfig.showWarning" = false;
           "browser.tabs.warnOnClose" = false;
           "media.videocontrols.picture-in-picture.video-toggle.enabled" = true;
@@ -84,6 +85,57 @@
           "gfx.webrender.all" = true;
           "network.http.http3.enabled" = true;
           "network.socket.ip_addr_any.disabled" = true; # disallow bind to 0.0.0.0
+
+          # fastfox
+          "gfx.content.skia-font-cache-size" = 32;
+          "gfx.canvas.accelerated.cache-items" = 32768;
+          "gfx.canvas.accelerated.cache-size" = 4096;
+          "webgl.max-size" = 16384;
+          "browser.cache.disk.enable" = false;
+          "browser.cache.memory.capacity" = 131072;
+          "browser.cache.memory.max_entry_size" = 20480;
+          "browser.sessionhistory.max_total_viewers" = 4;
+          "browser.sessionstore.max_tabs_undo" = 10;
+          "media.memory_cache_max_size" = 262144;
+          "media.memory_caches_combined_limit_kb" = 1048576;
+          "media.cache_readahead_limit" = 600;
+          "media.cache_resume_threshold" = 300;
+          "image.cache.size" = 10485760;
+          "image.mem.decode_bytes_at_a_time" = 65536;
+          "network.http.max-connections" = 1800;
+          "network.http.max-persistent-connections-per-server" = 10;
+          "network.http.request.max-start-delay" = 5;
+          "network.http.pacing.requests.enabled" = false;
+          "network.dnsCacheEntries" = 10000;
+          "network.dnsCacheExpiration" = 3600;
+          "network.ssl_tokens_cache_capacity" = 10240;
+          "network.http.speculative-parallel-limit" = 0;
+          "network.dns.disablePrefetch" = true;
+          "network.dns.disablePrefetchFromHTTPS" = true;
+          "browser.urlbar.speculativeConnect.enabled" = false;
+          "browser.places.speculativeConnect.enabled" = false;
+          "network.prefetch-next" = false;
+          "network.predictor.enabled" = false;
+
+          # securefox
+          "browser.contentblocking.category" = "strict";
+          "privacy.trackingprotection.allow_list.baseline.enabled" = true;
+          "privacy.trackingprotection.allow_list.convenience.enabled" = true;
+          "security.OCSP.enabled" = 0;
+          "security.pki.crlite_mode" = 2;
+          "browser.sessionstore.interval" = 60000;
+          "signon.formlessCapture.enabled" = false;
+          "signon.privateBrowsingCapture.enabled" = false;
+          "network.auth.subresource-http-auth-allow" = 1;
+          "editor.truncate_user_pastes" = false;
+          "network.http.referer.XOriginTrimmingPolicy" = 2;
+          "permissions.default.geo" = 2;
+          "permissions.default.desktop-notification" = 2;
+          "geo.provider.network.url" = "https://beacondb.net/v1/geolocate";
+          "browser.search.update" = false;
+          "permissions.manager.defaultsUrl" = "";
+          "browser.newtabpage.activity-stream.default.sites" = true;
+          "dom.text_fragments.create_text_fragment.enabled" = true;
         };
       };
     };
