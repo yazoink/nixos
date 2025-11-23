a9fdffa2269b514b5155cae3f2247998f2923aaf
yazoink <yazoink@firemail.cc>
2025-11-23 13:52:27 +1100

    tweak

--- modules/home-manager/features/nvim/default.nix
+++ modules/home-manager/features/nvim/default.nix
@@ -19,7 +19,7 @@ in {
         # Theme
         theme = {
           enable = true;
-          theme = "base16";
+          name = "base16";
           base16-colors = {
             base00 = "#${base16Scheme.base00}";
             base01 = "#${base16Scheme.base01}";
