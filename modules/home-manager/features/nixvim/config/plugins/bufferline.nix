ce1e1d8ae59d35a9603973a360d54c9d949ba361
yazoink <yazoink@firemail.cc>
2025-11-23 14:35:51 +1100

    tweak

--- modules/home-manager/features/nixvim/config/plugins/bufferline.nix
+++ modules/home-manager/features/nixvim/config/plugins/bufferline.nix
@@ -63,29 +63,23 @@ in {
         };
         separator = {
           fg = "#${config.stylix.base16Scheme.base00 or "000000"}";
-          bg = "${base16Scheme.base00}";
+          bg = "#${base16Scheme.base00}";
         };
         separator_selected = {
           fg = "#${config.stylix.base16Scheme.base00 or "000000"}";
-          bg = "${base16Scheme.base00}";
+          bg = "#${base16Scheme.base00}";
         };
         separator_visible = {
           fg = "#${config.stylix.base16Scheme.base00 or "000000"}";
-          bg = "${base16Scheme.base00}";
+          bg = "#${base16Scheme.base00}";
         };
         modified = {
           fg = "#${config.stylix.base16Scheme.base03 or "ffffff"}";
-          bg =
-            if (polarity == "dark")
-            then "#${base16Scheme.base00}"
-            else "none";
+          bg = "#${base16Scheme.base00}";
         };
         modified_visible = {
           fg = "#${config.stylix.base16Scheme.base00 or "000000"}";
-          bg =
-            if (polarity == "dark")
-            then "#${base16Scheme.base00}"
-            else "none";
+          bg = "#${base16Scheme.base00}";
         };
         modified_selected = {
           fg = "#${config.stylix.base16Scheme.base0B or "ffffff"}";
