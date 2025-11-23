f633a12c5af2bd4e990cccb7a1b240d74fbac27f
yazoink <yazoink@firemail.cc>
2025-11-23 13:33:00 +1100

    tweak

--- modules/home-manager/features/nvim/default.nix
+++ modules/home-manager/features/nvim/default.nix
@@ -16,122 +16,120 @@
     };
     programs.nvf = {
       enable = true;
-      vim = {
-        # Plugins
-        telescope.enable = true;
-        autocomplete.nvim-cmp.enable = true;
-        mini.statusline.enable = true;
+      # Plugins
+      telescope.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+      mini.statusline.enable = true;
 
-        session = {
-          nvim-session-manager.enable = false;
-        };
+      session = {
+        nvim-session-manager.enable = false;
+      };
 
-        gestures = {
-          gesture-nvim.enable = false;
-        };
+      gestures = {
+        gesture-nvim.enable = false;
+      };
 
-        comments = {
-          comment-nvim.enable = true;
-        };
+      comments = {
+        comment-nvim.enable = true;
+      };
 
-        spellcheck = {
+      spellcheck = {
+        enable = true;
+      };
+
+      filetree = {
+        neo-tree = {
           enable = true;
         };
+      };
 
-        filetree = {
-          neo-tree = {
-            enable = true;
-          };
-        };
+      tabline = {
+        nvimBufferline.enable = true;
+      };
 
-        tabline = {
-          nvimBufferline.enable = true;
-        };
+      treesitter.context.enable = true;
+      binds = {
+        whichKey.enable = true;
+        cheatsheet.enable = true;
+      };
 
-        treesitter.context.enable = true;
-        binds = {
-          whichKey.enable = true;
-          cheatsheet.enable = true;
-        };
+      git = {
+        enable = true;
+        gitsigns.enable = true;
+        gitsigns.codeActions.enable = false; # throws an annoying debug message
+      };
 
-        git = {
+      terminal = {
+        toggleterm = {
           enable = true;
-          gitsigns.enable = true;
-          gitsigns.codeActions.enable = false; # throws an annoying debug message
+          lazygit.enable = true;
         };
+      };
 
-        terminal = {
-          toggleterm = {
-            enable = true;
-            lazygit.enable = true;
-          };
-        };
+      # LSP
+      lsp = {
+        # This must be enabled for the language modules to hook into
+        # the LSP API.
+        enable = true;
 
-        # LSP
-        lsp = {
-          # This must be enabled for the language modules to hook into
-          # the LSP API.
+        formatOnSave = true;
+        lspkind.enable = false;
+        lightbulb.enable = true;
+        lspsaga.enable = false;
+        trouble.enable = true;
+      };
+
+      # Languages
+      languages = {
+        enableTreesitter = true;
+        enableFormat = true;
+
+        nix.enable = true;
+        ts.enable = true;
+        rust.enable = true;
+        sql.enable = true;
+        clang.enable = true;
+        python.enable = true;
+        markdown.enable = true;
+        php.enable = true;
+        lua.enable = true;
+        go.enable = true;
+        bash.enable = true;
+        css.enable = true;
+        html.enable = true;
+      };
+
+      # Snippets
+      snippets.luasnip.enable = true;
+
+      # UI
+      visuals = {
+        nvim-scrollbar.enable = true;
+        nvim-web-devicons.enable = true;
+        nvim-cursorline.enable = true;
+        cinnamon-nvim.enable = true;
+        fidget-nvim.enable = true;
+        highlight-undo.enable = true;
+        indent-blankline.enable = true;
+      };
+
+      ui = {
+        borders.enable = true;
+        noice.enable = true;
+        colorizer.enable = true;
+        modes-nvim.enable = false; # the theme looks terrible with catppuccin
+        illuminate.enable = true;
+        smartcolumn = {
           enable = true;
-
-          formatOnSave = true;
-          lspkind.enable = false;
-          lightbulb.enable = true;
-          lspsaga.enable = false;
-          trouble.enable = true;
-        };
-
-        # Languages
-        languages = {
-          enableTreesitter = true;
-          enableFormat = true;
-
-          nix.enable = true;
-          ts.enable = true;
-          rust.enable = true;
-          sql.enable = true;
-          clang.enable = true;
-          python.enable = true;
-          markdown.enable = true;
-          php.enable = true;
-          lua.enable = true;
-          go.enable = true;
-          bash.enable = true;
-          css.enable = true;
-          html.enable = true;
-        };
-
-        # Snippets
-        snippets.luasnip.enable = true;
-
-        # UI
-        visuals = {
-          nvim-scrollbar.enable = true;
-          nvim-web-devicons.enable = true;
-          nvim-cursorline.enable = true;
-          cinnamon-nvim.enable = true;
-          fidget-nvim.enable = true;
-          highlight-undo.enable = true;
-          indent-blankline.enable = true;
-        };
-
-        ui = {
-          borders.enable = true;
-          noice.enable = true;
-          colorizer.enable = true;
-          modes-nvim.enable = false; # the theme looks terrible with catppuccin
-          illuminate.enable = true;
-          smartcolumn = {
-            enable = true;
-            setupOpts.custom_colorcolumn = {
-              # this is a freeform module, it's `buftype = int;` for configuring column position
-              nix = "110";
-              ruby = "120";
-              java = "130";
-              go = ["90" "130"];
-            };
+          setupOpts.custom_colorcolumn = {
+            # this is a freeform module, it's `buftype = int;` for configuring column position
+            nix = "110";
+            ruby = "120";
+            java = "130";
+            go = ["90" "130"];
           };
-          fastaction.enable = true;
         };
+        fastaction.enable = true;
       };
     };
   };
