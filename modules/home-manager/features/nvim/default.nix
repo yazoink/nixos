163f522b1db5b3583176c898dd58559e2aed805e
yazoink <yazoink@firemail.cc>
2025-11-23 13:53:01 +1100

    tweak

--- modules/home-manager/features/nvim/default.nix
+++ modules/home-manager/features/nvim/default.nix
@@ -21,22 +21,22 @@ in {
           enable = true;
           name = "base16";
           base16-colors = {
-            base00 = "#${base16Scheme.base00}";
-            base01 = "#${base16Scheme.base01}";
-            base02 = "#${base16Scheme.base02}";
-            base03 = "#${base16Scheme.base03}";
-            base04 = "#${base16Scheme.base04}";
-            base05 = "#${base16Scheme.base05}";
-            base06 = "#${base16Scheme.base06}";
-            base07 = "#${base16Scheme.base07}";
-            base08 = "#${base16Scheme.base08}";
-            base09 = "#${base16Scheme.base09}";
-            base0A = "#${base16Scheme.base0A}";
-            base0B = "#${base16Scheme.base0B}";
-            base0C = "#${base16Scheme.base0C}";
-            base0D = "#${base16Scheme.base0D}";
-            base0E = "#${base16Scheme.base0E}";
-            base0F = "#${base16Scheme.base0F}";
+            base00 = "${base16Scheme.base00}";
+            base01 = "${base16Scheme.base01}";
+            base02 = "${base16Scheme.base02}";
+            base03 = "${base16Scheme.base03}";
+            base04 = "${base16Scheme.base04}";
+            base05 = "${base16Scheme.base05}";
+            base06 = "${base16Scheme.base06}";
+            base07 = "${base16Scheme.base07}";
+            base08 = "${base16Scheme.base08}";
+            base09 = "${base16Scheme.base09}";
+            base0A = "${base16Scheme.base0A}";
+            base0B = "${base16Scheme.base0B}";
+            base0C = "${base16Scheme.base0C}";
+            base0D = "${base16Scheme.base0D}";
+            base0E = "${base16Scheme.base0E}";
+            base0F = "${base16Scheme.base0F}";
           };
         };
 
