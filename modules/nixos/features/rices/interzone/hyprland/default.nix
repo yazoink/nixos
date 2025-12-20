99d462b4c5ddc0a1a57a207816c90708cec414b2
yazoink <yazoink@firemail.cc>
2025-12-20 13:58:05 +1100

    reformat

--- modules/nixos/features/rices/interzone/hyprland/default.nix
+++ modules/nixos/features/rices/interzone/hyprland/default.nix
@@ -4,9 +4,6 @@
   inputs,
   ...
 }: {
-  bundles.desktopBase = {
-    hyprlock.enable = true;
-  };
   nix.settings = {
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
