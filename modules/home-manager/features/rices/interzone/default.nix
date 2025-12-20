d1817cbcf13cc8b408de3cb70495507db6aebdea
yazoink <yazoink@firemail.cc>
2025-12-20 13:57:29 +1100

    reformat

--- modules/home-manager/features/rices/interzone/default.nix
+++ modules/home-manager/features/rices/interzone/default.nix
@@ -5,8 +5,10 @@
   pkgs,
   inputs,
   ...
-}: {
-  config = lib.mkMerge [
+}: let
+  inherit (config.myOptions.desktopTheme.rice) interzone;
+in {
+  config = lib.mkIf interzone.enable (lib.mkMerge [
     (import ./display-manager {inherit config osConfig lib pkgs;})
     (import ./eww {inherit config osConfig lib pkgs;})
     (import ./fnott {inherit config osConfig lib pkgs;})
@@ -15,5 +17,5 @@
     (import ./swayosd {inherit config osConfig lib pkgs;})
     (import ./walker {inherit config osConfig lib pkgs;})
     (import ./waybar {inherit config osConfig lib pkgs;})
-  ];
+  ]);
 }
