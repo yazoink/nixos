7a7fd11ca1b9bdcb4be4cfa3a892300a085b6d0e
yazoink <yazoink@firemail.cc>
2025-12-21 12:22:06 +1100

    tweak zathura

--- modules/home-manager/features/document-readers/zathura/default.nix
+++ modules/home-manager/features/document-readers/zathura/default.nix
@@ -10,6 +10,20 @@
       enable = true;
       extraConfig = ''
         set font "${config.stylix.fonts.sansSerif.name} ${builtins.toString config.stylix.fonts.sizes.applications}"
+        set selection-clipboard clipboard
+
+        map J zoom out
+        map K zoom in
+        unmap f
+        map f toggle_fullscreen
+        map [fullscreen] f toggle_fullscreen
+        map r reload
+
+        map b toggle_page_mode # Toggle dual page mode
+
+        set pages-per-row 1
+
+        set smooth-scroll "true"
       '';
     };
   };
