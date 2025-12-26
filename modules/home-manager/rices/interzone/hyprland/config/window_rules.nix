384abf4c1ccbba54f90082123e34d3bf3c66c060
yazoink <yazoink@firemail.cc>
2025-12-26 14:08:01 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/window_rules.nix
+++ modules/home-manager/rices/interzone/hyprland/config/window_rules.nix
@@ -1,16 +1,16 @@
 {
   windowrule = [
-    # "match:title ^(HyprViewBinds)$, float "
-    # "match:class ^(org.kde.kruler)$, float true"
-    # "match:title ^(galculator)$, float true"
-    # "match:title ^(Open File)(.*)$, float true"
-    # "match:title ^(Select a File)(.*)$, float true"
-    # "match:title ^(Open Folder)(.*)$, float true"
-    # "match:title ^(Save As)(.*)$, float true"
-    # "match:title ^(Picture-in-Picture)$, float true"
-    # "match:title ^(Picture-in-Picture)$, pin true"
-    # "match:title ^(Authentication Required)$, float true"
-    # "match:title ^(Soundboard)$, float true"
-    # "match:title ^((E|e)tterna)(.*)$, fullscreen true"
+    "match:title ^(HyprViewBinds)$, float "
+    "match:class ^(org.kde.kruler)$, float true"
+    "match:title ^(galculator)$, float true"
+    "match:title ^(Open File)(.*)$, float true"
+    "match:title ^(Select a File)(.*)$, float true"
+    "match:title ^(Open Folder)(.*)$, float true"
+    "match:title ^(Save As)(.*)$, float true"
+    "match:title ^(Picture-in-Picture)$, float true"
+    "match:title ^(Picture-in-Picture)$, pin true"
+    "match:title ^(Authentication Required)$, float true"
+    "match:title ^(Soundboard)$, float true"
+    "match:title ^((E|e)tterna)(.*)$, fullscreen true"
   ];
 }
