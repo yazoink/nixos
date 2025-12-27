e2c70a600461597e666d4dec03bbee656ccd0a4b
yazoink <yazoink@firemail.cc>
2025-12-27 17:45:41 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
+++ modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
@@ -13,9 +13,12 @@
       borders-plus-plus = {
         natural_rounding = "yes";
         add_borders = 1;
-        "col.border_1" = "rgb(${config.stylix.base16Scheme.base04})";
+        "col.border_1" = "rgb(${config.stylix.base16Scheme.base01})";
         border_size_1 = 1;
       };
     };
   };
+  windowrule = [
+    "match:focus true, hyprbars:title_color rgb(${config.stylix.base16Scheme.base05})"
+  ];
 }
