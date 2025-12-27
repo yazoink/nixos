b246ae1a883da8c037440a0f949a3858976f35b3
yazoink <yazoink@firemail.cc>
2025-12-27 16:12:19 +1100

    tweak

--- modules/home-manager/features/web-browsers/zen/default.nix
+++ modules/home-manager/features/web-browsers/zen/default.nix
@@ -64,6 +64,7 @@
           "zen.view.compact.animate-sidebar" = false;
           "zen.welcome-screen.seen" = true;
           "zen.urlbar.behavior" = "float";
+          "zen.theme.content-element-separation" = 0;
 
           "browser.aboutConfig.showWarning" = false;
           "browser.tabs.warnOnClose" = false;
