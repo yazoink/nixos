266c71bc6106ad00b1842ae272f200a9b9ed8dd7
yazoink <yazoink@firemail.cc>
2025-12-27 17:35:20 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
+++ modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
@@ -12,9 +12,9 @@
     plugin = {
       borders-plus-plus = {
         natural_rounding = "yes";
-        add_borders = 2;
-        col.border_2 = "rgb(ffffff)";
+        add_borders = 1;
         col.border_1 = "rgb(000000)";
+        col.border_2 = "rgb(ffffff)";
         border_size_1 = 5;
         border_size_2 = 3;
       };
