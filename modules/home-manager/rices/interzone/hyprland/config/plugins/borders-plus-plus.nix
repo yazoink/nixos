429bb02187390f39f6f6a66175dcddf154728032
yazoink <yazoink@firemail.cc>
2025-12-27 17:31:13 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
+++ modules/home-manager/rices/interzone/hyprland/config/plugins/borders-plus-plus.nix
@@ -15,7 +15,7 @@
         add_borders = 2;
         col.border_1 = "rgb(ffffff)";
         col.border_2 = "rgb(000000)";
-        border_size_1 = 3;
+        border_size_1 = 5;
         border_size_2 = 3;
       };
     };
