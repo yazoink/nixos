811aa2fb5ac0e09b447d5c861f01b7f812962f92
yazoink <yazoink@firemail.cc>
2026-01-20 19:23:11 +1100

    tweak

--- modules/nixos/features/mpd/default.nix
+++ modules/nixos/features/mpd/default.nix
@@ -18,19 +18,34 @@
       # Optional:
       network.listenAddress = "any"; # if you want to allow non-localhost connections
 
-      extraConfig = ''
-        audio_output {
-          type "pipewire"
-          name "My PipeWire Output" # this can be whatever you want
-        }
+      settings = {
+        audio_output = [
+          {
+            type = "pipewire";
+            name = "skjfdhskdjfh";
+          }
+          {
+            type = "fifo";
+            name = "my_fifo";
+            path = "/tmp/mpd/fifo";
+            format = "44100:16:2";
+          }
+        ];
+      };
 
-        audio_output {
-          type "fifo"
-          name "my_fifo"
-          path "/tmp/mpd.fifo"
-          format "44100:16:2"
-        }
-      '';
+      # extraConfig = ''
+      #   audio_output {
+      #     type "pipewire"
+      #     name "My PipeWire Output" # this can be whatever you want
+      #   }
+      #
+      #   audio_output {
+      #     type "fifo"
+      #     name "my_fifo"
+      #     path "/tmp/mpd.fifo"
+      #     format "44100:16:2"
+      #   }
+      # '';
     };
 
     systemd.services.mpd.environment = {
