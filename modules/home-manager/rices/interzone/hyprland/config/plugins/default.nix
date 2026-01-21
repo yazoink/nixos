a1f9979597474f761e7ca4db7e0207fd66425684
yazoink <yazoink@firemail.cc>
2026-01-21 19:05:31 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/plugins/default.nix
+++ modules/home-manager/rices/interzone/hyprland/config/plugins/default.nix
@@ -7,7 +7,7 @@
   ...
 }:
 lib.mkMerge [
-  (import ./hyprbars.nix {inherit pkgs config osConfig inputs;})
+  # (import ./hyprbars.nix {inherit pkgs config osConfig inputs;})
   # (import ./borders-plus-plus.nix {inherit pkgs config osConfig inputs;})
   # (import ./hyprexpo.nix {inherit pkgs config inputs;})
   # (import ./hyprgrass.nix {
