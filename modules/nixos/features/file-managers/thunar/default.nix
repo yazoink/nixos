5bd1fa590cb3de77f9d12bcaa4f638c07c26d823
yazoink <yazoink@firemail.cc>
2026-01-23 16:30:13 +1100

    tweak

--- modules/nixos/features/file-managers/thunar/default.nix
+++ modules/nixos/features/file-managers/thunar/default.nix
@@ -10,7 +10,7 @@
     programs = {
       thunar = {
         enable = true;
-        plugins = with pkgs.xfce; [
+        plugins = with pkgs; [
           thunar-archive-plugin
           thunar-volman
           thunar-media-tags-plugin
