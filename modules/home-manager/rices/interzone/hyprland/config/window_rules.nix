f03877753ec8c4e5384f982b39812dfb4d6afc7b
yazoink <yazoink@firemail.cc>
2026-01-24 23:11:25 +1100

    tweak

--- modules/home-manager/rices/interzone/hyprland/config/window_rules.nix
+++ modules/home-manager/rices/interzone/hyprland/config/window_rules.nix
@@ -1,6 +1,7 @@
 {
   windowrule = [
     "match:title ^(HyprViewBinds)$, float true"
+    "match:title ^(Rename)(.*)$, float true"
     "match:class ^(org.kde.kruler)$, float true"
     "match:title ^(galculator)$, float true"
     "match:title ^(Open File)(.*)$, float true"
