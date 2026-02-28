78aaf05794a092a8fd343ba191dce98b128a45f4
yazoink <yazoink@firemail.cc>
2026-02-28 11:53:35 +1100

    tweak

--- nixos/fluoride/options.nix
+++ nixos/fluoride/options.nix
@@ -76,7 +76,7 @@
       gamescope.enable = true;
       virtManager.enable = true;
       lutgen.enable = true;
-      # krita.enable = true;
+      krita.enable = true;
     };
     hardwareFeatures = {
       bootloader = {
