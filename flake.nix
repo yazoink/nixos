ec473ea08d2550a8a27a0e6180991151b8db503e
yazoink <yazoink@firemail.cc>
2026-05-30 13:04:46 +1000

    tweak

--- flake.nix
+++ flake.nix
@@ -4,13 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-25.11";
-    /*
-          home-manager = {
-          url = "github:nix-community/home-manager";
-          inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
-    */
-    home-manager.url = "github:nix-community/home-manager/release-26.05";
     stylix.url = "github:danth/stylix";
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
