5d71770c0420280c479130e4882e4a4fb6b0aa76
yazoink <yazoink@firemail.cc>
2026-06-07 12:28:34 +1000

    tweak

--- modules/home-manager/bundles/desktop-base/theme/gtk/default.nix
+++ modules/home-manager/bundles/desktop-base/theme/gtk/default.nix
@@ -16,7 +16,7 @@ in {
     gtk2 = {
       extraConfig = builtins.readFile gtkrcFile;
     };
-    gtk4.theme = config.gtk.theme; # might cause issues?
+    # gtk4.theme = config.gtk.theme; # might cause issues?
     # gtk3 = {
     #   extraConfig = {
     #     gtk-decoration-layout = "menu";
