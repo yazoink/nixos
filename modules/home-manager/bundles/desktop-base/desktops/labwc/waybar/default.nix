d16eebd92a4fda472ba8cc243abed346d5384d36
yazoink <yazoink@firemail.cc>
2026-06-09 02:55:06 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/waybar/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/waybar/default.nix
@@ -28,6 +28,8 @@
       "active-only": true,
       "tooltip": true,
       "tooltip-format": "Mod + 1-5 to switch workspaces",
+      "on-scroll-up": "${pkgs.wtype}/bin/wtype -M win -M shift -P left",
+      "on-scroll-down": "${pkgs.wtype}/bin/wtype -M win -M shift -P right",
       "format": "{icon}"
     }
   '';
