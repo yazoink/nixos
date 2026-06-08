d9a6a1adbaffb957c7f9cea9c554dc9986af5fc9
yazoink <yazoink@firemail.cc>
2026-06-09 04:01:03 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/waybar/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/waybar/default.nix
@@ -253,7 +253,7 @@
   barConfig = ''
     "reload_style_on_change": true,
     "layer": "top",
-    "mode": "overlay",
+    "mode": "dock",
     "exclusive": "true",
     "position": "${barPosition}",
     "spacing": 7,
