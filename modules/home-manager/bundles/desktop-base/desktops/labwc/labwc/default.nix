cb9327aa573a21b6959a037c82aedfb5bf449ee4
yazoink <yazoink@firemail.cc>
2026-06-09 02:05:32 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/default.nix
@@ -406,6 +406,25 @@ in {
               "@to" = 5;
             };
           }
+          # show next/prev workspace
+          {
+            "@key" = "W-S-Left";
+            action = {
+              "@name" = "GoToDesktop";
+              "@to" = "left";
+              "@wrap" = "yes";
+              "@toggle" = "no";
+            };
+          }
+          {
+            "@key" = "W-S-Right";
+            action = {
+              "@name" = "GoToDesktop";
+              "@to" = "right";
+              "@wrap" = "yes";
+              "@toggle" = "no";
+            };
+          }
           # show desktop
           {
             "@key" = "W-S-d";
@@ -514,6 +533,77 @@ in {
             action = {
               "@name" = "Execute";
               "@command" = "eww --config /home/${osConfig.myOptions.userAccount.username}/.config/widgets close-all; pkill walker";
+            };
+          }
+          # media keys
+          {
+            "@key" = "XF86AudioPlay";
+            action = {
+              "@name" = "Execute";
+              "@command" = "playerctl play-pause";
+            };
+          }
+          {
+            "@key" = "XF86AudioPause";
+            action = {
+              "@name" = "Execute";
+              "@command" = "playerctl play-pause";
+            };
+          }
+          {
+            "@key" = "XF86AudioNext";
+            action = {
+              "@name" = "Execute";
+              "@command" = "playerctl next";
+            };
+          }
+          {
+            "@key" = "XF86AudioPrev";
+            action = {
+              "@name" = "Execute";
+              "@command" = "playerctl previous";
+            };
+          }
+          {
+            "@key" = "XF86AudioMute";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --output-volume mute-toggle";
+            };
+          }
+          {
+            "@key" = "XF86AudioMicMute";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --input-volume mute-toggle";
+            };
+          }
+          {
+            "@key" = "XF86AudioRaiseVolume";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --output-volume raise";
+            };
+          }
+          {
+            "@key" = "XF86AudioLowerVolume";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --output-volume lower";
+            };
+          }
+          {
+            "@key" = "XF86MonBrightnessDown";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --brightness lower";
+            };
+          }
+          {
+            "@key" = "XF86MonBrightnessUp";
+            action = {
+              "@name" = "Execute";
+              "@command" = "swayosd-client --brightness raise";
             };
           }
         ];
