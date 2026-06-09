0bce464983a50430b25042a505312215400e68c7
yazoink <yazoink@firemail.cc>
2026-06-09 17:50:13 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/eww/config/scripts/confirm_prompt.sh
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/eww/config/scripts/confirm_prompt.sh
@@ -3,6 +3,7 @@
 echo "confirm_prompt.sh"
 config=$EWW_CONFIG_DIR
 eww --config "$config" close power
+eww --config "$config" open closer-window
 eww --config "$config" open confirm --arg thing="$1" --arg action="$2"
 
 if [[ $? == 0 ]]; then
@@ -11,6 +12,7 @@ if [[ $? == 0 ]]; then
         sleep 0.5
         eww --config "$config" active-windows | grep -q "confirm"
         if [[ $? != 0 ]]; then
+            eww --config "$config" close closer-window
             exit 0
         fi
     done
