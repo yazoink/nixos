d9d304c54b04d05b598966362128accac0e28ee2
yazoink <yazoink@firemail.cc>
2026-06-09 20:36:57 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/default.nix
@@ -54,13 +54,15 @@ in {
     window.titlebar.padding.height: 8
 
     # window border
-    window.active.border.color: #${base16Scheme.base01}
+    window.active.border.color: #${base16Scheme.base02}
     window.inactive.border.color: #${base16Scheme.base02}
 
     # ToggleKeybinds status indicator
     window.active.indicator.toggled-keybind.color: #${base16Scheme.base08}
 
     # window titlebar background
+    # window.active.title.bg.color: #${base16Scheme.base01}
+    # window.inactive.title.bg.color: #${base16Scheme.base01}
     window.active.title.bg.color: #${base16Scheme.base01}
     window.inactive.title.bg.color: #${base16Scheme.base01}
     window.*.title.bg: Solid
