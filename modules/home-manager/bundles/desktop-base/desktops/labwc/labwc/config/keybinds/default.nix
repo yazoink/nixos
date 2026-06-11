72ea03a93193ec1128916a667e15e2832e5c4f0e
yazoink <yazoink@firemail.cc>
2026-06-11 16:35:41 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/config/keybinds/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/labwc/config/keybinds/default.nix
@@ -12,6 +12,7 @@
       (import ./windows.nix {inherit config osConfig lib;})
       (import ./workspaces.nix {inherit config osConfig lib;})
       (import ./applications.nix {inherit config osConfig lib wallpaperCommand;})
+      (import ./media.nix {inherit config osConfig lib;})
     ];
   };
 }
