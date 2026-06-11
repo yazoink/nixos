a2f9f968ab0007279f54d52625377e4598424c8b
yazoink <yazoink@firemail.cc>
2026-06-11 15:41:16 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/kanshi/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/kanshi/default.nix
@@ -6,19 +6,21 @@
 }: {
   services.kanshi = {
     enable = true;
-    extraConfig = ''
-      profile {
-        output DP-1
-        mode 2560x1440@59.951000Hz
-        position 0,0
-        scale 1.35
-      }
-      profile {
-        output DP-2
-        mode 2560x1440@59.951000Hz
-        position 0,0
-        scale 1.35
-      }
-    '';
+    profiles = {
+      desktop = {
+        outputs = [
+          {
+            criteria = "DP-1";
+            scale = 1.35;
+            status = "enable";
+          }
+          {
+            criteria = "DP-2";
+            scale = 1.35;
+            status = "enable";
+          }
+        ];
+      };
+    };
   };
 }
