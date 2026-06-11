520b99811480befd8a75e10bfbcf739ee671f33e
yazoink <yazoink@firemail.cc>
2026-06-11 20:21:05 +1000

    stuff

--- modules/nixos/bundles/desktop-base/theme/themes/caroline/caroline-icons.nix
+++ modules/nixos/bundles/desktop-base/theme/themes/caroline/caroline-icons.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
     owner = "yazoink";
     repo = "caroline-icons";
     rev = "51fd705bd82bf2da45d3a35937d9d329759184a7";
-    sha256 = "";
+    sha256 = "sha256-scI4jpmcZnTHTSrY3gcR298Hcg+31PFwaife3zy7M/w=";
   };
 
   dontUnpack = true;
