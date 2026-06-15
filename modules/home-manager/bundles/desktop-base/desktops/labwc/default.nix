9afdc67d7f688802784f886d27d1ce8b0dd45f48
yazoink <yazoink@firemail.cc>
2026-06-15 20:28:35 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/default.nix
@@ -8,7 +8,16 @@
 }: let
   inherit (osConfig.myOptions.desktopTheme.rice) labwc;
 in {
+  options = {
+    desktopTheme.rice.labwc.barColour = lib.mkOption {
+      type = lib.types.string;
+      default = config.stylix.base16Scheme.base00;
+    };
+  };
   config = lib.mkIf labwc.enable (lib.mkMerge [
+    {
+      desktopTheme.rice.labwc.barColour = lib.mkIf (osConfig.myOptions.desktopTheme.colorscheme == "paradise") "101010";
+    }
     (import ./display-manager {inherit config osConfig lib pkgs;})
     (import ./eww {inherit config osConfig lib pkgs;})
     (import ./mako {inherit config osConfig lib pkgs;}) # either fnott or mako
