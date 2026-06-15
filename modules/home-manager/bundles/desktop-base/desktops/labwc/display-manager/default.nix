d3cb9761f7bbfc42352bae9fe1bd5c96e3804cb0
yazoink <yazoink@firemail.cc>
2026-06-15 19:31:02 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/display-manager/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/display-manager/default.nix
@@ -2,7 +2,7 @@
   home.file.".zprofile" = {
     text = ''
       if [[ -z "$DISPLAY" ]] && [[ $(tty) = /dev/tty1 ]]; then
-        exec labwc
+        exec dbus-run-session labwc
       fi
     '';
     executable = true;
