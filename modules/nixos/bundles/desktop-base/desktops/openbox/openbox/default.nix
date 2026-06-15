648f67601e8a5f843f8e487884e0eb0055bcce4d
yazoink <yazoink@firemail.cc>
2026-06-16 03:04:34 +1000

    stuff

--- modules/nixos/bundles/desktop-base/desktops/openbox/openbox/default.nix
+++ modules/nixos/bundles/desktop-base/desktops/openbox/openbox/default.nix
@@ -1,9 +1,20 @@
 # relies on openbox home-manager module
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [xinit libnotify xorg-server];
+  environment.systemPackages = with pkgs; [
+    xinit
+    libnotify
+    xorg-server
+    xf86-input-evdev
+    xf86-input-mouse
+    xf86-input-wacom
+    xf86-input-synaptics
+    xf86-video-intel
+    xf86-video-ati
+  ];
   services.xserver = {
     enable = true;
     windowManager.openbox.enable = true;
+    displayManager.startx.enable = true;
     layout = "us";
   };
   services = {
