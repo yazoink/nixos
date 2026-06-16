45299648b41bd8112f69d110af3b03c88134da9a
yazoink <yazoink@firemail.cc>
2026-06-16 12:03:03 +1000

    stuff

--- modules/nixos/bundles/desktop-base/desktops/labwc/default.nix
+++ modules/nixos/bundles/desktop-base/desktops/labwc/default.nix
@@ -6,10 +6,9 @@
 }: let
   inherit (config.myOptions.desktopTheme.rice) labwc labwc-glass;
 in {
-  config =
-    lib.mkIf labwc.enable
-    || labwc-glass.enable (lib.mkMerge [
-      (import ./labwc {inherit pkgs;})
-      (import ./hyprlock {})
-    ]);
+  config = lib.mkIf (labwc.enable
+    || labwc-glass.enable) (lib.mkMerge [
+    (import ./labwc {inherit pkgs;})
+    (import ./hyprlock {})
+  ]);
 }
