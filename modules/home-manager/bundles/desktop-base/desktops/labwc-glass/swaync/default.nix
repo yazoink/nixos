2e3fb7c9328447ddad60ed06a1ff7769ec270dbe
yazoink <yazoink@firemail.cc>
2026-06-16 17:46:20 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc-glass/swaync/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc-glass/swaync/default.nix
@@ -42,7 +42,7 @@
       hide-on-clear = false;
       hide-on-action = true;
       script-fail-notify = true;
-      widgets = ["title" "mpris" "dnd"];
+      widgets = ["title" "mpris" "dnd" "notifications"];
       widget-config = {
         title = {
           text = "Notifications";
