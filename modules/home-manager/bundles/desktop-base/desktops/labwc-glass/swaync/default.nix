a4917aa34cda708ee908277adf2c37ae8e8d6453
yazoink <yazoink@firemail.cc>
2026-06-16 19:37:52 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc-glass/swaync/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc-glass/swaync/default.nix
@@ -26,6 +26,7 @@
       control-center-margin-left = 4;
       control-center-height = 600;
       control-center-width = 450;
+      notification-window-width = 325;
       fit-to-screen = false;
       notification-2fa-action = true;
       notification-inline-replies = false;
