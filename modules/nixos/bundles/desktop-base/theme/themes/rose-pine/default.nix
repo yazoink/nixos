04275f95bed3d53f90bb56ed4bcf0ea2f700cc41
yazoink <yazoink@firemail.cc>
2026-06-22 15:44:01 +1000

    stuff

--- modules/nixos/bundles/desktop-base/theme/themes/rose-pine/default.nix
+++ modules/nixos/bundles/desktop-base/theme/themes/rose-pine/default.nix
@@ -19,12 +19,32 @@ in {
         package = pkgs.rose-pine-cursor;
         size = 32;
       };
-      base16Scheme = {
+      /*
+        base16Scheme = {
         base00 = "191724";
         base01 = "262432";
         base02 = "343240";
         base03 = "413F4E";
         base04 = "838193";
+        base05 = "E0DEF4";
+        base06 = "E0DEF4";
+        base07 = "56526E";
+        base08 = "EB6F92";
+        base09 = "F6C177";
+        base0A = "EA9A97";
+        base0B = "3E8FB0";
+        base0C = "9CCFD8";
+        base0D = "C4A7E7";
+        base0E = "F6C177";
+        base0F = "56526E";
+      };
+      */
+      base16Scheme = {
+        base00 = "191724";
+        base01 = "1F1D2E";
+        base02 = "26233A";
+        base03 = "403D52";
+        base04 = "524F67";
         base05 = "E0DEF4";
         base06 = "E0DEF4";
         base07 = "56526E";
