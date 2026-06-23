2043d648f1a6b9af945a631c6eeba017d02f887d
yazoink <yazoink@firemail.cc>
2026-06-23 23:42:10 +1000

    stuff

--- modules/nixos/bundles/desktop-base/theme/themes/rose-pine/default.nix
+++ modules/nixos/bundles/desktop-base/theme/themes/rose-pine/default.nix
@@ -21,6 +21,25 @@ in {
       };
       base16Scheme = {
         base00 = "191724";
+        base01 = "23212F";
+        base02 = "2E2C3A";
+        base03 = "383645";
+        base04 = "434150";
+        base05 = "E0DEF4";
+        base06 = "E0DEF4";
+        base07 = "56526E";
+        base08 = "EB6F92";
+        base09 = "F6C177";
+        base0A = "EA9A97";
+        base0B = "3E8FB0";
+        base0C = "9CCFD8";
+        base0D = "C4A7E7";
+        base0E = "F6C177";
+        base0F = "56526E";
+      };
+      /*
+        base16Scheme = {
+        base00 = "191724";
         base01 = "2E2C3A";
         base02 = "434150";
         base03 = "434150";
@@ -37,6 +56,7 @@ in {
         base0E = "F6C177";
         base0F = "56526E";
       };
+      */
       /*
         base16Scheme = {
         base00 = "191724";
