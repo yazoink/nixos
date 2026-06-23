0f46cdd502a51f8955594e3b924f71a4304b04d9
yazoink <yazoink@firemail.cc>
2026-06-23 21:06:37 +1000

    a

--- modules/nixos/bundles/desktop-base/theme/fonts/terminal/kosugi-maru/default.nix
+++ modules/nixos/bundles/desktop-base/theme/fonts/terminal/kosugi-maru/default.nix
@@ -7,7 +7,7 @@
   config = lib.mkIf (config.myOptions.bundles.desktopBase.enable && config.myOptions.desktopTheme.fonts.terminal.name == "Kosugi Maru") {
     stylix = {
       fonts = {
-        sansSerif = {
+        monospace = {
           package = pkgs.callPackage ./font.nix {};
           name = "Kosugi Maru";
         };
