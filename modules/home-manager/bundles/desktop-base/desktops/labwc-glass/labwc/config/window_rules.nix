5e86318d1415e156c0c470d3d1663a6b26ce4258
yazoink <yazoink@firemail.cc>
2026-06-24 02:28:15 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc-glass/labwc/config/window_rules.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc-glass/labwc/config/window_rules.nix
@@ -5,6 +5,22 @@
         "@identifier" = "imv";
         "@serverDecoration" = "yes";
       }
+      {
+        "@identifier" = "BleachBit";
+        "@serverDecoration" = "yes";
+      }
+      {
+        "@identifier" = "wdisplays";
+        "@serverDecoration" = "yes";
+      }
+      {
+        "@identifier" = "Fonts";
+        "@serverDecoration" = "yes";
+      }
+      {
+        "@identifier" = "Nicotine+";
+        "@serverDecoration" = "yes";
+      }
     ];
   };
 }
