968fea9f2c21e6d32afcab09dfbd2a8916c3c5da
yazoink <yazoink@firemail.cc>
2026-06-25 17:36:17 +1000

    a

---
+++ modules/home-manager/bundles/desktop-base/desktops/hyprland/hyprland-flat/default.nix
@@ -0,0 +1,24 @@
+{
+  config,
+  osConfig,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: let
+  inherit (osConfig.myOptions.desktopTheme.rice) hyprland;
+in {
+  config = lib.mkIf hyprland.enable (lib.mkMerge [
+    (import ./display-manager {inherit config osConfig lib pkgs;})
+    (import ./eww {inherit config osConfig lib pkgs;})
+    # (import ./fnott {inherit config osConfig lib pkgs;})
+    (import ./mako {inherit config osConfig lib pkgs;}) # either fnott or mako
+    (import ./hypridle {inherit config osConfig lib pkgs;})
+    (import ./hyprlock {inherit config osConfig lib pkgs;})
+    (import ./hyprland {inherit config osConfig lib pkgs inputs;})
+    (import ./swayosd {inherit config osConfig lib pkgs;})
+    (import ./walker {inherit config osConfig lib pkgs;})
+    # (import ./wofi {inherit config osConfig lib pkgs;}) # eithet walker or wofi
+    (import ./waybar {inherit config osConfig lib pkgs;})
+  ]);
+}
