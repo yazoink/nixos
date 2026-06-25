067e44c339d0d2515ffdc4cdcc491db1f28b3e60
yazoink <yazoink@firemail.cc>
2026-06-26 00:38:20 +1000

    a

--- modules/nixos/bundles/desktop-full/default.nix
+++ modules/nixos/bundles/desktop-full/default.nix
@@ -7,6 +7,9 @@
   ...
 }:
 lib.mkIf config.myOptions.bundles.desktopFull.enable (lib.mkMerge [
+  {
+    myOptions.bundles.desktopBase.enable = true;
+  }
   (import ./gamemode {inherit config lib pkgs inputs pkgs-stable;})
   (import ./mpd {inherit config lib pkgs inputs pkgs-stable;})
   (import ./mullvad {inherit config lib pkgs inputs pkgs-stable;})
