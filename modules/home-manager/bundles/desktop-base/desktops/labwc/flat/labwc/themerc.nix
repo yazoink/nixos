a764e6165a3a1159916de661825f2594883bd927
yazoink <yazoink@firemail.cc>
2026-06-26 17:13:22 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/themerc.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/themerc.nix
@@ -68,8 +68,8 @@ in {
     # window drop-shadows
     window.active.shadow.size: 60
     window.inactive.shadow.size: 40
-    window.active.shadow.color: #00000060
-    window.inactive.shadow.color: #00000040
+    window.active.shadow.color: #00000040
+    window.inactive.shadow.color: #00000030
 
     # Note that "menu", "iconify", "max", "close" buttons colors can be defined
     # individually by inserting the type after the button node, for example:
