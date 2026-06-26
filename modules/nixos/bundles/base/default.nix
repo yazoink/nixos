c85399e3e0c6ff667af5e7cf7cc1f20ee0e417ae
yazoink <yazoink@firemail.cc>
2026-06-26 23:26:50 +1000

    stuff

--- modules/nixos/bundles/base/default.nix
+++ modules/nixos/bundles/base/default.nix
@@ -5,19 +5,60 @@
   inputs,
   pkgs-stable,
   ...
-}: let
-  inherit (config.myOptions.bundles) desktopBase;
-in
-  lib.mkIf desktopBase.enable (lib.mkMerge [
-    {
-      myOptions.bundles.base.enable = lib.mkForce true;
-    }
-    (import ./acpi {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./audio {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./dconf {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./file-managers {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./fonts {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./gnome-keyring {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./polkit {inherit config lib pkgs inputs pkgs-stable;})
-    (import ./desktops {inherit config lib pkgs inputs pkgs-stable;})
-  ])
+}:
+lib.mkIf config.myOptions.bundles.base.enable (lib.mkMerge [
+  {
+    security.wrappers."mount.cifs" = {
+      program = "mount.cifs";
+      source = "${lib.getBin pkgs.cifs-utils}/bin/mount.cifs";
+      owner = "root";
+      group = "root";
+      setuid = true;
+    };
+
+    environment.systemPackages = with pkgs; [
+      ##### System Info #####
+      pciutils
+      lshw
+      inxi
+      dmidecode
+      lm_sensors
+      usbutils
+      # procps
+      ncdu
+
+      ##### Utils #####
+      vim
+      git
+      wget
+      curl
+      psmisc
+      coreutils-full
+      ntfs3g
+      util-linux
+      bc
+      ripgrep
+
+      ##### Compression/Decompression #####
+      zip
+      unzip
+      p7zip
+      rar
+
+      ##### Samba #####
+      cifs-utils
+      # mtpfs
+      ##### Misc #####
+      linux-firmware
+    ];
+  }
+  (import ./avahi {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./bootloader {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./kernel {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./locale {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./network {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./nix {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./sops {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./ssh {inherit config lib pkgs inputs pkgs-stable;})
+  (import ./user {inherit config lib pkgs inputs pkgs-stable;})
+])
