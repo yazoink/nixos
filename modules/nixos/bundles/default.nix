50970632774175706ecd49a6bd2870b04f5e862b
yazoink <yazoink@firemail.cc>
2026-06-26 22:49:20 +1000

    stuff

--- modules/nixos/bundles/default.nix
+++ modules/nixos/bundles/default.nix
@@ -13,12 +13,12 @@ in
       (import ./base {inherit config lib pkgs inputs pkgs-stable;}))
 
     (lib.mkIf bundles.desktopBase.enable (lib.mkMerge [
-      {bundles.base.enable = true;}
+      (import ./base {inherit config lib pkgs inputs pkgs-stable;})
       (import ./desktop-base {inherit config lib pkgs inputs pkgs-stable;})
     ]))
 
     (lib.mkIf bundles.desktopFull.enable (lib.mkMerge [
-      {bundles.desktopBase.enable = true;}
+      (import ./desktop-base {inherit config lib pkgs inputs pkgs-stable;})
       (import ./desktop-full {inherit config lib pkgs inputs pkgs-stable;})
     ]))
   ]
