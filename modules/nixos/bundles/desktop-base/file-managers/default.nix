5ffd11c31ee4439dff04b1a843d93328b52836e3
yazoink <yazoink@firemail.cc>
2026-06-26 21:39:56 +1000

    stuff

--- modules/nixos/bundles/desktop-base/file-managers/default.nix
+++ modules/nixos/bundles/desktop-base/file-managers/default.nix
@@ -4,5 +4,6 @@
   config,
   ...
 }: let
-  name = builtins.trace "${config.myOptions.defaultApps.fileManager}" config.myOptions.defaultApps.fileManager;
-in (import (./. + "/${name}") {inherit lib pkgs;})
+  name = config.myOptions.defaultApps.fileManager;
+in
+  lib.mkMerge [(import (./. + "/${name}") {inherit lib pkgs;})]
