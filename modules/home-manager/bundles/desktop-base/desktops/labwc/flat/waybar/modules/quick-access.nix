1e266b011b0347a3db58ea81ddacde2f65a49511
yazoink <yazoink@firemail.cc>
2026-06-29 16:43:13 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/waybar/modules/quick-access.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/waybar/modules/quick-access.nix
@@ -2,35 +2,37 @@
   laptop,
   lib,
   ...
-}: (lib.mkMerge [
-  (lib.mkIf laptop.enable ''
-    "group/quick-access": {
-      "orientation": "inherit",
-      "modules": [
-      "group/volume-expander",
-      "group/backlight-expander",
-      "group/battery-expander",
-      "group/tray-expander"
-      ]
+}:
+if laptop.enable
+then ''
+  "group/quick-access": {
+    "orientation": "inherit",
+    "modules": [
+    "group/volume-expander",
+    "group/backlight-expander",
+    "group/battery-expander",
+    "group/tray-expander"
+    ]
+  },
+  "custom/left-arrow": {
+    "format": "",
+    "tooltip": false
+  },
+  "group/quick-access-slider": {
+    "orientation": "inherit",
+    "drawer": {
+      "click-to-reveal": true,
+      "transition-duration": 1000,
+      "children-class": "tray-group-item"
     },
-    "custom/left-arrow": {
-      "format": "",
-      "tooltip": false
-    },
-    "group/quick-access-slider": {
-      "orientation": "inherit",
-      "drawer": {
-        "click-to-reveal": true,
-        "transition-duration": 1000,
-        "children-class": "tray-group-item"
-      },
-      "modules": [
-        "custom/left-arrow",
-        "group/quick-access"
-      ]
-    }
-  '')
-  (lib.mkIf (laptop.enable == false) ''
+    "modules": [
+      "custom/left-arrow",
+      "group/quick-access"
+    ]
+  }
+''
+else
+  lib.mkIf (laptop.enable == false) ''
     "group/quick-access": {
       "orientation": "inherit",
       "modules": [
@@ -54,5 +56,4 @@
         "group/quick-access"
       ]
     }
-  '')
-])
+  ''
