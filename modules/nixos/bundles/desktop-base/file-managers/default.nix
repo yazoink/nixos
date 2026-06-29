3f26c92b788ebe2e3407e7b2a91dd319c5c63caa
yazoink <yazoink@firemail.cc>
2026-06-29 14:26:50 +1000

    stuff

--- modules/nixos/bundles/desktop-base/file-managers/default.nix
+++ modules/nixos/bundles/desktop-base/file-managers/default.nix
@@ -5,13 +5,4 @@
   ...
 }: let
   name = config.myOptions.defaultApps.fileManager;
-in
-  (import ./. "/${name}")
-  lib.mkMerge [
-    (lib.mkIf (fileManager == "nemo")
-      (import ./nemo {inherit pkgs;}))
-    (lib.mkIf (fileManager == "pcmanfm")
-      (import ./pcmanfm {inherit pkgs;}))
-    (lib.mkIf (fileManager == "thunar")
-      (import ./thunar {inherit pkgs;}))
-  ]
+in (import ./. "/${name}")
