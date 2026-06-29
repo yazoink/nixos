6eaa8866d98795a66d04ae3a4de4f0334b47379b
yazoink <yazoink@firemail.cc>
2026-06-29 16:05:06 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/common/stylix/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/common/stylix/default.nix
@@ -18,4 +18,26 @@ lib.mkMerge [
   (import ./cava {inherit config pkgs osConfig lib;})
   (import ./vesktop {inherit config pkgs osConfig lib;})
   (import ./lutgen {inherit config pkgs osConfig lib;})
+  {
+    xdg.configFile."colors.css".text = with config.lib.stylix.colors; ''
+      @define-color base00 #${base00};
+      @define-color base01 #${base01};
+      @define-color base02 #${base02};
+      @define-color base03 #${base03};
+      @define-color base04 #${base04};
+      @define-color base05 #${base05};
+      @define-color base06 #${base06};
+      @define-color base07 #${base07};
+      @define-color base08 #${base08};
+      @define-color base09 #${base09};
+      @define-color base0A #${base0A};
+      @define-color base0B #${base0B};
+      @define-color base0C #${base0C};
+      @define-color base0D #${base0D};
+      @define-color base0E #${base0E};
+      @define-color base0F #${base0F};
+
+      @define-color surface rgba(${base05-rgb-r}, ${base05-rgb-g}, ${base05-rgb-b}, 0.09);
+    '';
+  }
 ]
