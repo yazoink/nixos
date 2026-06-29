a153a5a2547f7ee80e2f9d58ef8d341db919d27f
yazoink <yazoink@firemail.cc>
2026-06-29 14:21:07 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/file-managers/default.nix
+++ modules/home-manager/bundles/desktop-base/file-managers/default.nix
@@ -1,21 +1,10 @@
-{
-  osConfig,
-  lib,
-  pkgs,
-  config,
-  inputs,
-  ...
-}: let
+{osConfig, ...}: let
   name = osConfig.myOptions.defaultApps.fileManager;
-  makeCfg = name: desktopFile:
-    lib.mkMerge [
-      (import (./. + "/${name}") {inherit osConfig config lib pkgs inputs;})
-      {
-        xdg.mimeApps.defaultApplications = {
-          "inode/directory" = [desktopFile]; # Directories
-        };
-      }
-    ];
+  makeCfg = name: desktopFile: {
+    xdg.mimeApps.defaultApplications = {
+      "inode/directory" = [desktopFile]; # Directories
+    };
+  };
   desktopFiles = {
     nemo = "nemo.desktop";
     pcmanfm = "pcmanfm.desktop";
