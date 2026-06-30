966d546e767b3e45d3f2d6022dc6a914f30cbea3
yazoink <yazoink@firemail.cc>
2026-07-01 00:36:43 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/common/hypridle/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/common/hypridle/default.nix
@@ -3,27 +3,27 @@
   pkgs,
   ...
 }: {
-  home.packages = with pkgs; [brightnessctl];
   services.hypridle = builtins.trace "hypridle enabled" {
     enable = true;
     settings = {
       general = {
-        after_sleep_cmd = "${lib.getExe pkgs.wlopm} --on \\*";
         ignore_dbus_inhibit = false;
-        lock_cmd = "hyprlock";
+        lock_cmd = "pidof hyprlock || hyprlock";
+        after_sleep_cmd = "${lib.getExe pkgs.wlopm} --on \\*";
+        before_sleep_cmd = "loginctl lock-session";
       };
       listener = [
         {
-          timeout = 900;
+          timeout = 300; # 5 mins
           on-timeout = "${lib.getExe pkgs.chayang} -d 15 && ${lib.getExe pkgs.wlopm} --off \\*";
           on-resume = "${lib.getExe pkgs.wlopm} --on \\*";
         }
         {
-          timeout = 1200;
-          on-timeout = "${lib.getExe pkgs.hyprlock}";
+          timeout = 330;
+          on-timeout = "loginctl lock-session";
         }
         {
-          timeout = 5000;
+          timeout = 1800; # 30 mins
           on-timeout = "systemctl suspend";
         }
       ];
