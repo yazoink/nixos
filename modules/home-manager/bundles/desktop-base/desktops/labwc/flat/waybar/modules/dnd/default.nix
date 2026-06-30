84c44aaed32b38a5cd706c47b95b7a0a2250760f
yazoink <yazoink@firemail.cc>
2026-06-30 16:56:58 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/waybar/modules/dnd/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/waybar/modules/dnd/default.nix
@@ -4,7 +4,7 @@
     "tooltip": true,
     "tooltip-format": "Do not disturb (Mod+Shift+D)",
     "exec-on-event": true,
-    "interval": 3600,
+    "interval": 60,
     "on-click": "bash ${../../../../common/scripts/dnd.sh}; pkill -SIGRTMIN+8 waybar",
     "signal": 8,
     "exec": "bash ${./dnd-icon.sh}"
