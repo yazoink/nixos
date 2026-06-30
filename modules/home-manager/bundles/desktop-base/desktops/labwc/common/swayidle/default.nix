563b6faa83c666c0c6660e6a5e4892e632b6fe81
yazoink <yazoink@firemail.cc>
2026-06-30 12:58:12 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/common/swayidle/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/common/swayidle/default.nix
@@ -8,12 +8,12 @@
     timeouts = [
       {
         timeout = 1000;
-        command = "${lib.getExe pkgs.hyprlock}";
+        command = "${lib.getExe pkgs.chayang} -d 15 && ${lib.getExe pkgs.wlopm} --off *";
+        resumeCommand = "${lib.getExe pkgs.wlopm} --on *";
       }
       {
         timeout = 1500;
-        command = "${lib.getExe pkgs.wlopm} --off *";
-        resumeCommand = "${lib.getExe pkgs.wlopm} --on *";
+        command = "${lib.getExe pkgs.hyprlock}";
       }
       {
         timeout = 10000;
