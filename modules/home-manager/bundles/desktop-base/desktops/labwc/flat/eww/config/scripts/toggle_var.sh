2734f51068ea4e27836bade4a7e8e8fd8f8ff9ed
yazoink <yazoink@firemail.cc>
2026-07-05 00:13:56 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/eww/config/scripts/toggle_var.sh
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/eww/config/scripts/toggle_var.sh
@@ -8,40 +8,48 @@ else
     sleep=false
     case "$1" in
         "music_visible")
-            eww update monitor_visible=false; \
-                eww update weather_visible=false; \
+            if [[ $(eww get monitor_visible) == true ]] || [[ $(eww get weather_visible) == true ]]; then
                 sleep=true
+            fi
+            eww update monitor_visible=false
+            eww update weather_visible=false
             ;;
         "monitor_visible")
-            eww update music_visible=false; \
-                eww update weather_visible=false; \
+            if [[ $(eww get music_visible) == true ]] || [[ $(eww get weather_visible) == true ]]; then
                 sleep=true
+            fi
+            eww update music_visible=false
+            eww update weather_visible=false
+            sleep=true
             ;;
         "weather_visible")
-            eww update music_visible=false; \
-                eww update monitor_visible=false; \
-                eww poll weather_tz weather_temp weather_apparent_temp weather_icon weather_colour; \
+            if [[ $(eww get monitor_visible) == true ]] || [[ $(eww get music_visible) == true ]]; then
                 sleep=true
+            fi
+            eww update music_visible=false
+            eww update monitor_visible=false
+            eww poll weather_tz weather_temp weather_apparent_temp weather_icon weather_colour
+            sleep=true
             ;;
         "lock_selected")
-            eww update exit_selected=false; \
-                eww update reboot_selected=false; \
-                eww update shutdown_selected=false
+            eww update exit_selected=false
+            eww update reboot_selected=false
+            eww update shutdown_selected=false
             ;;
         "exit_selected")
-            eww update lock_selected=false; \
-                eww update reboot_selected=false; \
-                eww update shutdown_selected=false
+            eww update lock_selected=false
+            eww update reboot_selected=false
+            eww update shutdown_selected=false
             ;;
         "reboot_selected")
-            eww update lock_selected=false; \
-                eww update exit_selected=false; \
-                eww update shutdown_selected=false
+            eww update lock_selected=false
+            eww update exit_selected=false
+            eww update shutdown_selected=false
             ;;
         "shutdown_selected")
-            eww update lock_selected=false; \
-                eww update exit_selected=false; \
-                eww update reboot_selected=false
+            eww update lock_selected=false
+            eww update exit_selected=false
+            eww update reboot_selected=false
             ;;
     esac
     [ $sleep == true ] && sleep 0.6
