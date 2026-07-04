f871b0a04a117f19a51dde8a23f96daf8f860d51
yazoink <yazoink@firemail.cc>
2026-07-05 01:13:45 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/eww/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/eww/default.nix
@@ -7,14 +7,64 @@
   inherit (config.lib.stylix) colors;
   inherit (osConfig.myOptions) defaultApps;
   dollarSign = "$";
+  tempCmd =
+    if (config.networking.hostName == "stardust")
+    then "sensors | grep Tctl | awk '{print $2}' | sed 's/\\+//g;s/\\.[0-9]°C//g'"
+    else "sensors | grep 'Core 0' | awk '{print $3}' | sed 's/\\+//g;s/\\.[0-9]°C//g'";
 in {
-  home.packages = with pkgs; [eww font-awesome jq curl];
+  home.packages = with pkgs; [eww font-awesome jq curl lm_sensors];
   xdg.configFile = {
     "eww" = {
       source = ./config;
       recursive = true;
       force = true;
     };
+    "eww/widgets/monitor.yuck".text = ''
+      (defpoll cpu :run-while monitor_visible :interval "17s" "bash ./scripts/cpu.sh")
+      (defpoll temp :run-while monitor_visible :interval "17s"  `${tempCmd}`)
+
+      (defwidget monitor []
+        (revealer
+          :transition "slideup"
+          :reveal monitor_visible
+          (box
+            :class "monitor-box"
+            :orientation "v"
+            :spacing 8
+            (box
+              :orientation "h"
+              :space-evenly true
+              :spacing 8
+              (box
+                :class "monitor-item cpu base08"
+                (label :text "" :class "icon")
+                (label :text "${dollarSign}{round(cpu, 0)}%" :class "usage")
+              )
+              (box
+                :class "monitor-item ram base0A"
+                (label :text "" :class "icon")
+                (label :text "${dollarSign}{round(EWW_RAM.used_mem_perc, 0)}%" :class "usage")
+              )
+            )
+            (box
+              :orientation "h"
+              :space-evenly true
+              :spacing 8
+              (box
+                :class "monitor-item disk base0B"
+                (label :text "" :class "icon")
+                (label :text "${dollarSign}{round(EWW_DISK["/"].used_perc, 0)}%" :class "usage")
+              )
+              (box
+                :class "monitor-item temp base0D"
+                (label :text "" :class "icon")
+                (label :text "${dollarSign}{temp}°C" :class "usage")
+              )
+            )
+          )
+        )
+      )
+    '';
     "eww/widgets/launchers.yuck".text = with defaultApps; ''
       (defvar dnd false)
       (defpoll :run-while control_panel_visible dnd_icon :interval "17s"  "bash ./scripts/dnd_icon.sh")
