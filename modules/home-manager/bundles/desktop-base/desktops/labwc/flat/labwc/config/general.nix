234234c5d66047f353321dcca96fa57d15bc8d0f
yazoink <yazoink@firemail.cc>
2026-07-05 21:11:40 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/config/general.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/config/general.nix
@@ -2,6 +2,12 @@
   desktops = {
     "@number" = 5;
   };
+  margin = {
+    "@top" = 0;
+    "@bottom" = 0;
+    "@left" = 0;
+    "@right" = 0;
+  };
   focus = {
     followMouse = "yes";
     raiseOnFocus = "no";
