21054ae8a1afb8a3f4058aca586fb0a345378c47
yazoink <yazoink@firemail.cc>
2026-07-05 19:07:53 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/themerc.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/labwc/themerc.nix
@@ -10,8 +10,8 @@ in {
     # The global padding.{width,height} of openbox are not supported because
     # the default labwc button geometry deviates from that of openbox
     #
-    window.titlebar.padding.width: 12
-    window.titlebar.padding.height: 12
+    window.titlebar.padding.width: 8
+    window.titlebar.padding.height: 8
 
     # window border
     window.active.border.color: #${base01}
