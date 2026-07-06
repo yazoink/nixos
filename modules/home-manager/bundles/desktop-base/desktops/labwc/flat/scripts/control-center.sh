b78d558f4385fc8393e9447b1589c1e591fdc412
yazoink <yazoink@firemail.cc>
2026-07-07 02:26:31 +1000

    a

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/scripts/control-center.sh
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/scripts/control-center.sh
@@ -5,6 +5,8 @@ v="$(eww get control_center_visible)"
 if [ $v == true ]; then
     eww update control_center_visible=false
 else
+    "$HOME"/.config/eww/control_center/scripts/get_apps.py update
+    "$HOME"/.config/eww/control_center/scripts/get_apps.py onchange ""
     eww update control_center_visible=true
 fi
 
