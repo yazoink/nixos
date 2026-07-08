a9bc3528247fee12cf72e1df6a6015f623dc660a
yazoink <yazoink@firemail.cc>
2026-07-08 22:08:08 +1000

    stuff

--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/default.nix
@@ -29,7 +29,7 @@ lib.mkMerge [
   (import ./hyprlock {inherit config osConfig lib pkgs;})
   # (import ./hypridle {inherit config osConfig lib pkgs;})
   (import ./labwc {inherit config osConfig lib pkgs;})
-  (import ./mako {inherit config osConfig lib pkgs;})
+  # (import ./mako {inherit config osConfig lib pkgs;})
   (import ./swayosd {inherit config osConfig lib pkgs;})
   (import ./waybar {inherit config osConfig lib pkgs;})
   (import ./wofi {inherit config osConfig lib pkgs;})
--- modules/home-manager/bundles/desktop-base/desktops/labwc/flat/mako/default.nix
+++ modules/home-manager/bundles/desktop-base/desktops/labwc/flat/mako/default.nix
@@ -14,22 +14,21 @@ in {
   stylix.targets.mako.enable = false;
   services.mako = with colors; {
     enable = true;
-    /*
-      settings = {
+    settings = {
       layer = "overlay";
       actions = "true";
       icons = "true";
       anchor = "bottom-right";
-      border-radius = "18";
+      border-radius = "16";
       background-color = "#${base00}";
       border-color = "#${base01}";
       text-color = "#${base05}";
       progress-color = "over #${base05}";
       border-size = "1";
       font = "${font.name} ${toString font.size}";
-      padding = "15";
-      margin = "5,0,0";
-      outer-margin = "5";
+      padding = "16";
+      margin = "4,0,0";
+      outer-margin = "4";
       default-timeout = "5000";
       # icon-path = "/run/current-system/sw/share/icons/${iconTheme.name}";
       "mode=do-not-disturb" = {
